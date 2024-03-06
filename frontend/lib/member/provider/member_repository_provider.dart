52d10f518231f5a023a897bdf186d27b80718e22
jia5232 <jia5232@naver.com>
2024-03-06 16:38:41 +0900

    feat: member repository provider를 추가한다.

---
+++ frontend/lib/member/provider/member_repository_provider.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../common/const/data.dart';
+import '../../common/provider/dio_provider.dart';
+import '../model/member_model.dart';
+
+final memberRepositoryProvider = Provider<MemberRepository>(
+      (ref) {
+    final dio = ref.watch(dioProvider);
+
+    return MemberRepository(baseUrl: 'http://$ip/member', dio: dio);
+  },
+);
+
+class MemberRepository{
+  final String baseUrl;
+  final Dio dio;
+
+  MemberRepository({
+    required this.baseUrl,
+    required this.dio,
+});
+
+  Future<MemberModel> getMe() async {
+    final resp = await dio.get(
+      '$baseUrl/api/users/member',
+      options: Options(
+        headers: {
+          'refreshToken': 'true',
+        },
+      ),
+    );
+
+    return MemberModel.fromJson(resp.data);
+  }
+}
