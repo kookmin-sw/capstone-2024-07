a1e2e68070f05c868ff3ba0083340148346e9edf
Woongbae Yun <devbelly@naver.com>
2024-03-06 15:16:19 +0900

    fix : 누락된 인터페이스를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/domain/comment/CommentRepository.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/comment/CommentRepository.kt
@@ -2,7 +2,7 @@ package com.dclass.backend.domain.comment
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CommentRepository : JpaRepository<Comment, Long> {
-    fun findCommentByIdAndUserId(commentId: Long, userId: Long) : Comment?
+interface CommentRepository : JpaRepository<Comment, Long>, CommentRepositorySupport {
+    fun findCommentByIdAndUserId(commentId: Long, userId: Long): Comment?
     fun findAllByUserId(userId: Long): List<Comment>
 }
