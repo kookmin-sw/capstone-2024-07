091481f1d2a6c32afadfc095dc01c54e9c8bbbb6
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-07 14:17:00 +0900

    feat: edit password 비즈니스 로직을 구현한다

--- backend/src/main/kotlin/com/dclass/backend/application/UserService.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/UserService.kt
@@ -1,9 +1,11 @@
 package com.dclass.backend.application
 
+import com.dclass.backend.application.dto.EditPasswordRequest
 import com.dclass.backend.application.dto.ResetPasswordRequest
 import com.dclass.backend.domain.user.User
 import com.dclass.backend.domain.user.UserRepository
 import com.dclass.backend.domain.user.findByEmail
+import com.dclass.backend.domain.user.getOrThrow
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -22,6 +24,11 @@ class UserService(
         val user = getByEmail(request.email)
         user.resetPassword(request.name, passwordGenerator.generate())
         userRepository.save(user)
+    }
+
+    fun editPassword(id: Long, request: EditPasswordRequest) {
+        require(request.password == request.confirmPassword) { "새 비밀번호가 일치하지 않습니다." }
+        userRepository.getOrThrow(id).changePassword(request.oldPassword, request.password)
     }
 
 }
