1d9ff2b3b0386610cb93044e19b2a1f6cc4b8b6a
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-07 13:06:57 +0900

    feat: password reset 비즈니스 로직을 구현한다

--- backend/src/main/kotlin/com/dclass/backend/application/UserService.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/UserService.kt
@@ -1,5 +1,6 @@
 package com.dclass.backend.application
 
+import com.dclass.backend.application.dto.ResetPasswordRequest
 import com.dclass.backend.domain.user.User
 import com.dclass.backend.domain.user.UserRepository
 import com.dclass.backend.domain.user.findByEmail
@@ -10,9 +11,17 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional
 @Service
 class UserService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val passwordGenerator: PasswordGenerator,
 ) {
     fun getByEmail(email: String): User {
         return userRepository.findByEmail(email) ?: throw IllegalArgumentException("회원이 존재하지 않습니다. email: $email")
     }
+
+    fun resetPassword(request: ResetPasswordRequest) {
+        val user = getByEmail(request.email)
+        user.resetPassword(request.name, passwordGenerator.generate())
+        userRepository.save(user)
+    }
+
 }
