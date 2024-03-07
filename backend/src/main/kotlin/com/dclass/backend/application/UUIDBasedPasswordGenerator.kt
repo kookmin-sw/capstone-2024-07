d3c6b562d42134e26faf5da39b434337fe6d74f9
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-07 12:58:07 +0900

    feat: UUID로 구현한 PasswordGenerator를 등록한다

---
+++ backend/src/main/kotlin/com/dclass/backend/application/UUIDBasedPasswordGenerator.kt
@@ -0,0 +1,11 @@
+package com.dclass.backend.application
+
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class UUIDBasedPasswordGenerator: PasswordGenerator {
+    override fun generate(): String {
+        return UUID.randomUUID().toString().take(0)
+    }
+}
