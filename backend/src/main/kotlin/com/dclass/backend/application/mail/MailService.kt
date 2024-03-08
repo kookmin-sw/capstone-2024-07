a7a707d1c3b1767ec62ea71450440c8cc391f1e7
Woongbae Yun <devbelly@naver.com>
2024-03-08 13:10:12 +0900

    feat : 비밀번호 초기화 이벤트를 받아 메일을 발송한다

--- backend/src/main/kotlin/com/dclass/backend/application/mail/MailService.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/mail/MailService.kt
@@ -1,9 +1,11 @@
 package com.dclass.backend.application.mail
 
+import com.dclass.backend.domain.user.PasswordResetEvent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.springframework.stereotype.Service
+import org.springframework.transaction.event.TransactionalEventListener
 import org.thymeleaf.context.Context
 import org.thymeleaf.spring6.ISpringTemplateEngine
 
@@ -22,6 +24,25 @@ class MailService(
                 email,
                 "메일 인증 코드를 발송해 드립니다. ",
                 templateEngine.process("mail/email-authentication.html", context)
+            )
+        }
+
+    @TransactionalEventListener
+    fun sendPasswordResetMail(event: PasswordResetEvent) =
+        CoroutineScope(Dispatchers.IO).launch {
+            val context = Context().apply {
+                setVariables(
+                    mapOf(
+                        "name" to event.name,
+                        "password" to event.password
+                    )
+                )
+            }
+
+            mailSender.send(
+                event.email,
+                "${event.name}님, 임시 비밀번호를 발송해 드립니다.",
+                templateEngine.process("mail/password-reset.html", context)
             )
         }
 }
