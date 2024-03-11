037dd67b309903573a51fb0aa9e97e5f221ca436
Woongbae Yun <devbelly@naver.com>
2024-03-12 00:01:48 +0900

    feat : 이미지 S3 키값을 통해 이미지 주소를 반환하도록 변경한다

--- backend/src/main/kotlin/com/dclass/backend/application/PostService.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/PostService.kt
@@ -2,6 +2,11 @@ package com.dclass.backend.application
 
 import com.dclass.backend.application.dto.PostResponse
 import com.dclass.backend.domain.post.PostRepository
+import com.dclass.backend.infra.s3.AwsPresigner
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -9,11 +14,16 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional
 class PostService(
     private val postValidator: PostValidator,
-    private val postRepository: PostRepository
+    private val postRepository: PostRepository,
+    private val awsPresigner: AwsPresigner
 ) {
     fun getById(userId: Long, postId: Long): PostResponse {
         postValidator.validateGetPost(userId, postId)
 
-        return postRepository.findPostById(postId)
+        return postRepository.findPostById(postId).apply {
+            images = runBlocking(Dispatchers.IO) {
+                images.map { async { awsPresigner.getPostObjectPresigned(it) } }.awaitAll()
+            }
+        }
     }
 }
