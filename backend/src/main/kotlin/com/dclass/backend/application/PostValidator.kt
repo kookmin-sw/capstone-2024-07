b3816747bc4660e22a443b43abd733430760496c
Woongbae Yun <devbelly@naver.com>
2024-03-11 18:13:08 +0900

    feat : 게시글을 불러올 수 있는지 검증하는 로직을 추가한다

--- backend/src/main/kotlin/com/dclass/backend/application/PostValidator.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/PostValidator.kt
@@ -1,7 +1,8 @@
 package com.dclass.backend.application
 
-import com.dclass.backend.domain.community.CommunityRepository
 import com.dclass.backend.domain.belong.BelongRepository
+import com.dclass.backend.domain.community.CommunityRepository
+import com.dclass.backend.domain.post.PostRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -10,11 +11,20 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional
 class PostValidator(
     private val belongRepository: BelongRepository,
-    private val communityRepository: CommunityRepository
+    private val communityRepository: CommunityRepository,
+    private val postRepository: PostRepository
 ) {
     fun validateCreatePost(userId: Long, communityId: Long) {
         val belong = belongRepository.findByUserId(userId)
-        val community = communityRepository.findByIdOrNull(communityId)
-        check(belong.contain(communityId)) { "해당 커뮤니티에 게시글을 작성할 수 없습니다." }
+        val community = communityRepository.findByIdOrNull(communityId)!!
+        check(belong.contain(community.departmentId)) { "해당 커뮤니티에 게시글을 작성할 수 없습니다." }
+    }
+
+    fun validateGetPost(userId: Long, postId: Long) {
+        val belong = belongRepository.findByUserId(userId)
+        val post = postRepository.findByIdOrNull(postId)
+            ?: throw IllegalArgumentException("해당 게시글이 존재하지 않습니다.")
+        val community = communityRepository.findByIdOrNull(post.communityId)!!
+        check(belong.contain(community.departmentId)) { "해당 커뮤니티의 게시글을 조회할 수 없습니다." }
     }
 }
