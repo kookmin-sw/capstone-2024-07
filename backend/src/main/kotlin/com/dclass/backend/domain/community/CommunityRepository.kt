d3192dbd0e1cf4866fd5a1d0f553df17495877f3
Woongbae Yun <devbelly@naver.com>
2024-03-12 16:17:04 +0900

    feat : 특정 학과에 속하는 커뮤니티를 찾는 쿼리를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/domain/community/CommunityRepository.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/community/CommunityRepository.kt
@@ -3,4 +3,5 @@ package com.dclass.backend.domain.community
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CommunityRepository : JpaRepository<Community, Long> {
+    fun findByDepartmentIdIn(departmentIds: List<Long>): List<Community>
 }
