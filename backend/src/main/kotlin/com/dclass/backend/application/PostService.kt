5db8cf03a281e09507608ba5c379964c6baa0377
Woongbae Yun <devbelly@naver.com>
2024-03-12 16:34:27 +0900

    feat : 자신이 속한 학과에 해당하는 전체 게시글을 가져오는 기능을 추가한다

--- backend/src/main/kotlin/com/dclass/backend/application/PostService.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/PostService.kt
@@ -1,6 +1,9 @@
 package com.dclass.backend.application
 
 import com.dclass.backend.application.dto.PostResponse
+import com.dclass.backend.application.dto.PostScrollPageRequest
+import com.dclass.backend.domain.belong.BelongRepository
+import com.dclass.backend.domain.community.CommunityRepository
 import com.dclass.backend.domain.post.PostRepository
 import com.dclass.backend.infra.s3.AwsPresigner
 import kotlinx.coroutines.Dispatchers
@@ -15,8 +18,22 @@ import org.springframework.transaction.annotation.Transactional
 class PostService(
     private val postValidator: PostValidator,
     private val postRepository: PostRepository,
+    private val belongRepository: BelongRepository,
+    private val communityRepository: CommunityRepository,
     private val awsPresigner: AwsPresigner
 ) {
+    fun getAll(userId: Long, request: PostScrollPageRequest): List<PostResponse> {
+        val departmentIds = belongRepository.findByUserId(userId).departmentIds
+        val communityIds = communityRepository.findByDepartmentIdIn(departmentIds)
+            .map { it.id }
+
+        return postRepository.findPostScrollPage(communityIds, request).onEach {
+            it.images = runBlocking(Dispatchers.IO) {
+                it.images.map { async { awsPresigner.getPostObjectPresigned(it) } }.awaitAll()
+            }
+        }
+    }
+
     fun getById(userId: Long, postId: Long): PostResponse {
         postValidator.validateGetPost(userId, postId)
 
