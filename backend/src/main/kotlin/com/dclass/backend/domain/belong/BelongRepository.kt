17b87e2e88059e7a5576dd729790415bed23303f
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-13 17:04:24 +0900

    feat: 조회쿼리에 예외처리를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/domain/belong/BelongRepository.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/belong/BelongRepository.kt
@@ -2,6 +2,9 @@ package com.dclass.backend.domain.belong
 
 import org.springframework.data.jpa.repository.JpaRepository
 
+fun BelongRepository.getOrThrow(userId: Long): Belong = findByUserId(userId)
+    ?: throw NoSuchElementException("소속이 존재하지 않습니다. userId: $userId")
+
 interface BelongRepository : JpaRepository<Belong, Long> {
-    fun findByUserId(userId: Long): Belong
+    fun findByUserId(userId: Long): Belong?
 }
--- backend/src/main/kotlin/com/dclass/backend/domain/department/DepartmentRepository.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/department/DepartmentRepository.kt
@@ -2,4 +2,9 @@ package com.dclass.backend.domain.department
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface DepartmentRepository : JpaRepository<Department, Long>
+fun DepartmentRepository.getOrThrow(title: String): Department = findByTitle(title)
+    ?: throw NoSuchElementException("부서가 존재하지 않습니다. title: $title")
+
+interface DepartmentRepository : JpaRepository<Department, Long> {
+    fun findByTitle(title: String): Department?
+}
