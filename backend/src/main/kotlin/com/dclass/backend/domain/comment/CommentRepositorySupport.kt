5d16af02cb94e5dae8fd6fa64d4c024d35cb7fd1
Woongbae Yun <devbelly@naver.com>
2024-03-13 17:02:52 +0900

    feat : 댓글과 대댓글의 갯수를 조회하는 쿼리를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/domain/comment/CommentRepositorySupport.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/comment/CommentRepositorySupport.kt
@@ -1,6 +1,7 @@
 package com.dclass.backend.domain.comment
 
 import com.dclass.backend.application.dto.CommentWithUserResponse
+import com.dclass.backend.domain.reply.Reply
 import com.dclass.backend.domain.user.User
 import com.linecorp.kotlinjdsl.dsl.jpql.jpql
 import com.linecorp.kotlinjdsl.render.jpql.JpqlRenderContext
@@ -9,6 +10,7 @@ import jakarta.persistence.EntityManager
 
 interface CommentRepositorySupport {
     fun findCommentWithUserByPostId(postId: Long): List<CommentWithUserResponse>
+    fun countCommentReplyByPostId(postId: Long): Long
 }
 
 class CommentRepositoryImpl(
@@ -30,5 +32,30 @@ class CommentRepositoryImpl(
         }
 
         return em.createQuery(query, context).resultList
+    }
+
+    override fun countCommentReplyByPostId(postId: Long): Long {
+        val query = jpql {
+            val replyCount = expression(Long::class, "cnt")
+
+            val subquery = select(
+                count(entity(Reply::class))
+            ).from(
+                entity(Comment::class),
+                join(Reply::class).on(path(Comment::id).eq(path(Reply::commentId)))
+            ).where(
+                path(Comment::postId).eq(postId)
+            ).asSubquery().`as`(replyCount)
+
+            select(
+                count(path(Comment::id)).plus(subquery)
+            ).from(
+                entity(Comment::class)
+            ).where(
+                path(Comment::postId).eq(postId)
+            )
+        }
+
+        return em.createQuery(query, context).singleResult
     }
 }
