2ce97361ef828ee0d1e547afef4c20ad89b9212f
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-14 17:14:14 +0900

    feat: 댓글관련 API 명세를 작성한다

--- backend/src/main/kotlin/com/dclass/backend/application/dto/CommentDtos.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/dto/CommentDtos.kt
@@ -4,6 +4,7 @@ import com.dclass.backend.domain.comment.Comment
 import com.dclass.backend.domain.comment.CommentLikes
 import com.dclass.backend.domain.user.User
 import com.dclass.backend.domain.user.UserInformation
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotNull
 import java.time.LocalDateTime
 
@@ -31,11 +32,40 @@ data class DeleteCommentRequest(
 )
 
 data class CommentResponse(
+    @Schema(
+        description = "댓글의 고유 식별자",
+        example = "1"
+    )
     val id: Long,
+
+    @Schema(
+        description = "댓글을 작성한 유저의 고유 식별자",
+        example = "1"
+    )
     val userId: Long,
+
+    @Schema(
+        description = "댓글이 달린 게시글의 고유 식별자",
+        example = "1"
+    )
     val postId: Long,
+
+    @Schema(
+        description = "댓글의 내용",
+        example = "댓글 내용"
+    )
     val content: String,
+
+    @Schema(
+        description = "댓글이 작성된 시각",
+        example = "2021-08-01T00:00:00"
+    )
     val createdDateTime: LocalDateTime,
+
+    @Schema(
+        description = "댓글이 수정된 시각",
+        example = "2021-08-01T00:00:00"
+    )
     val modifiedDateTime: LocalDateTime,
 ) {
     constructor(comment: Comment) : this(
@@ -70,13 +100,52 @@ data class CommentWithUserResponse(
 }
 
 data class CommentReplyWithUserResponse(
+    @Schema(
+        description = "댓글의 고유 식별자",
+        example = "1"
+    )
     val id: Long,
+
+    @Schema(
+        description = "댓글을 작성한 유저의 정보",
+        example = "1"
+    )
     val userInformation: UserInformation,
+
+    @Schema(
+        description = "댓글이 달린 게시글의 고유 식별자",
+        example = "1"
+    )
     val postId: Long,
+
+    @Schema(
+        description = "댓글의 내용",
+        example = "댓글 내용"
+    )
     val content: String,
+
+    @Schema(
+        description = "댓글의 좋아요 수",
+        example = "1"
+    )
     val likeCount: CommentLikes,
+
+    @Schema(
+        description = "댓글의 좋아요 여부",
+        example = "true"
+    )
     val isLiked: Boolean,
+
+    @Schema(
+        description = "댓글이 작성된 시각",
+        example = "2021-08-01T00:00:00"
+    )
     val createdAt: LocalDateTime,
+
+    @Schema(
+        description = "댓글의 답글 목록",
+        example = "1"
+    )
     val replies: List<ReplyWithUserResponse>
 ) {
     constructor(
