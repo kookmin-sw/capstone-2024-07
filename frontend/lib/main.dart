44644bb96d2a0ff57c40c4be28c0ea885658b7b2
jia5232 <jia5232@naver.com>
2024-03-14 23:50:44 +0900

    feat: RootTab 삭제하고 로그인 후 첫페이지를 board list screen으로 변경

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:frontend/common/view/root_tab.dart';
-import 'package:frontend/member/view/login_screen.dart';
-import 'package:frontend/member/view/signup_screen.dart';
 
 import 'common/provider/router_provider.dart';
 
