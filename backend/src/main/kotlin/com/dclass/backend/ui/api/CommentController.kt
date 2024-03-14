c641acea3d55dc440c3647941d49f490d6c65b6f
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-14 17:14:14 +0900

    feat: 댓글관련 API 명세를 작성한다

--- backend/src/main/kotlin/com/dclass/backend/ui/api/CommentController.kt
+++ backend/src/main/kotlin/com/dclass/backend/ui/api/CommentController.kt
@@ -4,16 +4,22 @@ import com.dclass.backend.application.CommentService
 import com.dclass.backend.application.dto.*
 import com.dclass.backend.domain.user.User
 import com.dclass.backend.security.LoginUser
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "Comment", description = "댓글 관련 API 명세")
 @RequestMapping("/api/comments")
 @RestController
 class CommentController(
     private val commentService: CommentService
 ) {
 
+    @Operation(summary = "댓글 생성 API", description = "게시글에 댓글을 생성합니다.")
+    @ApiResponse(responseCode = "200", description = "댓글 생성 성공")
     @PostMapping("/{postId}")
     fun createComment(
         @LoginUser user: User,
@@ -24,6 +30,8 @@ class CommentController(
         return ResponseEntity.ok(ApiResponses.success(comment))
     }
 
+    @Operation(summary = "댓글 수정 API", description = "댓글을 수정합니다.")
+    @ApiResponse(responseCode = "204", description = "댓글 수정 성공")
     @PutMapping("/{commentId}")
     fun updateComment(
         @LoginUser user: User,
@@ -34,6 +42,8 @@ class CommentController(
         return ResponseEntity.noContent().build()
     }
 
+    @Operation(summary = "댓글 삭제 API", description = "댓글을 삭제합니다.")
+    @ApiResponse(responseCode = "204", description = "댓글 삭제 성공")
     @DeleteMapping("/{commentId}")
     fun deleteComment(
         @LoginUser user: User,
@@ -43,6 +53,8 @@ class CommentController(
         return ResponseEntity.noContent().build()
     }
 
+    @Operation(summary = "댓글 조회 API", description = "게시글에 달린 댓글을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "댓글 조회 성공")
     @GetMapping("/{postId}")
     fun getComments(
         @LoginUser user: User,
