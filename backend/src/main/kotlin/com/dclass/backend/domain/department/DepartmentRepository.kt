7b03a347c6938adeb601c4fea1be574cb77aad47
Woongbae Yun <devbelly@naver.com>
2024-03-14 12:47:45 +0900

    refactor : 메서드 명을 변경한다

--- backend/src/main/kotlin/com/dclass/backend/domain/department/DepartmentRepository.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/department/DepartmentRepository.kt
@@ -2,7 +2,7 @@ package com.dclass.backend.domain.department
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-fun DepartmentRepository.getOrThrow(title: String): Department = findByTitle(title)
+fun DepartmentRepository.getByTitleOrThrow(title: String): Department = findByTitle(title)
     ?: throw NoSuchElementException("부서가 존재하지 않습니다. title: $title")
 
 interface DepartmentRepository : JpaRepository<Department, Long> {
