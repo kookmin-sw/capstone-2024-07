1f4875d60be931c479c5958919dd159e0d2e0c8d
Woongbae Yun <devbelly@naver.com>
2024-03-15 14:11:32 +0900

    feat : 전체 게시글 조회 API를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/ui/api/PostController.kt
+++ backend/src/main/kotlin/com/dclass/backend/ui/api/PostController.kt
@@ -2,6 +2,7 @@ package com.dclass.backend.ui.api
 
 import com.dclass.backend.application.PostService
 import com.dclass.backend.application.dto.PostResponse
+import com.dclass.backend.application.dto.PostScrollPageRequest
 import com.dclass.backend.domain.user.User
 import com.dclass.backend.security.LoginUser
 import org.springframework.http.ResponseEntity
@@ -24,5 +25,13 @@ class PostController(
     ): ResponseEntity<ApiResponses<PostResponse>> {
         val postResponse = postService.getById(user.id, postId)
         return ResponseEntity.ok(ApiResponses.success(postResponse))
+    }
+
+    @GetMapping
+    fun getPosts(
+        @LoginUser user: User,
+        request: PostScrollPageRequest
+    ): ResponseEntity<ApiResponses<List<PostResponse>>> {
+        return ResponseEntity.ok(ApiResponses.success(postService.getAll(user.id, request)))
     }
 }
