b7491274063a649835a466fad5c622b2b52f2ecd
Woongbae Yun <devbelly@naver.com>
2024-03-18 10:59:42 +0900

    feat : CommunityType을 추가한다

---
+++ backend/src/main/kotlin/com/dclass/backend/domain/community/CommunityType.kt
@@ -0,0 +1,19 @@
+package com.dclass.backend.domain.community
+
+import com.dclass.backend.exception.community.CommunityException
+import com.dclass.backend.exception.community.CommunityExceptionType.NOT_FOUND_COMMUNITY
+
+
+enum class CommunityType(val value: String) {
+    FREE("자유게시판"), GRADUATE("대학원게시판"), JOB("취준게시판"), STUDY("스터디모집"), QUESTION("질문게시판"), PROMOTION("홍보게시판");
+
+    companion object {
+        fun from(value: String?): CommunityType? {
+            if (value.isNullOrBlank()) {
+                return null;
+            }
+            return enumValues<CommunityType>().find { it.value == value }
+                ?: throw CommunityException(NOT_FOUND_COMMUNITY)
+        }
+    }
+}
