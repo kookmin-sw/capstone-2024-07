e484bdb708fc393f10bfdf63738492e5836314db
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-18 14:40:39 +0900

    refactor: reply 조회 방법을 수정한다

--- backend/src/main/kotlin/com/dclass/backend/application/ReplyService.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/ReplyService.kt
@@ -6,6 +6,7 @@ import com.dclass.backend.application.dto.ReplyResponse
 import com.dclass.backend.application.dto.UpdateReplyRequest
 import com.dclass.backend.domain.reply.Reply
 import com.dclass.backend.domain.reply.ReplyRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -20,21 +21,26 @@ class ReplyService(
     }
 
     fun update(userId: Long, request: UpdateReplyRequest) {
-        val reply = find(request.replyId, userId)
-        reply.changeContent(request.content)
+        val reply = find(request.replyId)
+        reply.changeContent(request.content, userId)
     }
 
     fun delete(userId: Long, request: DeleteReplyRequest) {
-        val reply = find(request.replyId, userId)
+        val reply = find(request.replyId)
+        check(reply.userId == userId) {
+            "자신이 작성한 대댓글만 삭제할 수 있습니다."
+        }
         replyRepository.delete(reply)
     }
 
-    private fun find(replyId: Long, userId: Long): Reply {
-        val reply = replyRepository.findReplyByIdAndUserId(replyId, userId)
-            ?: throw NoSuchElementException("해당 댓글이 존재하지 않습니다.")
+
+    private fun find(replyId: Long): Reply {
+        val reply = replyRepository.findByIdOrNull(replyId)
+            ?: throw NoSuchElementException("해당 대댓글이 존재하지 않습니다.")
         check(!reply.isDeleted(replyId)) {
-            "삭제된 댓글입니다."
+            "삭제된 대댓글입니다."
         }
         return reply
     }
+
 }
