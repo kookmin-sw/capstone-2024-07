4c159aba474f93219c9011691d4b1e264b100480
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-18 17:24:26 +0900

    feat: 학과 변경 비즈니스 로직을 구현한다

--- backend/src/main/kotlin/com/dclass/backend/application/BelongService.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/BelongService.kt
@@ -1,9 +1,11 @@
 package com.dclass.backend.application
 
 import com.dclass.backend.application.dto.SwitchDepartmentResponse
+import com.dclass.backend.application.dto.UpdateDepartmentRequest
 import com.dclass.backend.domain.belong.BelongRepository
 import com.dclass.backend.domain.belong.getOrThrow
 import com.dclass.backend.domain.department.DepartmentRepository
+import com.dclass.backend.domain.department.getByTitleOrThrow
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -14,6 +16,13 @@ class BelongService(
     private val departmentRepository: DepartmentRepository,
     private val belongRepository: BelongRepository,
 ) {
+    fun editDepartments(userId: Long, request: UpdateDepartmentRequest) {
+        val majorDepartment = departmentRepository.getByTitleOrThrow(request.major)
+        val minorDepartment = departmentRepository.getByTitleOrThrow(request.minor)
+
+        belongRepository.getOrThrow(userId).update(listOf(majorDepartment.id, minorDepartment.id))
+    }
+
     fun switchDepartment(userId: Long): SwitchDepartmentResponse {
         val belong = belongRepository.getOrThrow(userId)
         belong.switch()
