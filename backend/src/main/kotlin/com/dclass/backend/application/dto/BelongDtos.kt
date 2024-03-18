85480a36acf1a419022af5c335f4464f67c7483c
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-18 17:14:57 +0900

    feat: Belong 관련 Dto를 생성한다

---
+++ backend/src/main/kotlin/com/dclass/backend/application/dto/BelongDtos.kt
@@ -0,0 +1,8 @@
+package com.dclass.backend.application.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class SwitchDepartmentResponse(
+    @Schema(description = "활성화된 학과 이름", example = "컴퓨터공학과")
+    val activated: String,
+)
