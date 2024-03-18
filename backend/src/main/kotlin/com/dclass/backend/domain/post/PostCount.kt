0d9e27c1e02a9187e8749e9d16c67ec6ccfa0098
Woongbae Yun <devbelly@naver.com>
2024-03-18 16:37:10 +0900

    fix : 게시글 카운트 값 객체를 수정한다

--- backend/src/main/kotlin/com/dclass/backend/domain/post/PostCount.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/post/PostCount.kt
@@ -4,11 +4,12 @@ import jakarta.persistence.Embeddable
 
 @Embeddable
 class PostCount(
-    val viewCount: Int = 0,
-    val likeCount: Int = 0,
-    val commentReplyCount: Int = 0
+    val commentReplyCount: Int = 0,
+    val likeCount: Int = 0
 ) {
-    fun increaseViewCount(cnt: Int) = PostCount(viewCount + cnt, likeCount, commentReplyCount)
-    fun increaseLikeCount() = PostCount(viewCount, likeCount + 1, commentReplyCount)
-    fun increaseCommentReplyCount(cnt : Int) = PostCount(viewCount, likeCount, commentReplyCount + cnt)
+    fun increaseCommentReplyCount(cnt: Int) =
+        PostCount(commentReplyCount + cnt,likeCount)
+
+    fun syncLikeCount(cnt: Int) =
+        PostCount(commentReplyCount,cnt)
 }
