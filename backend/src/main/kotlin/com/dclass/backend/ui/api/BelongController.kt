145a7a3dae5124e8838be223ef9eb8b5b7b92d23
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-18 17:25:29 +0900

    feat: 학과 변경 API를 구현한다

--- backend/src/main/kotlin/com/dclass/backend/ui/api/BelongController.kt
+++ backend/src/main/kotlin/com/dclass/backend/ui/api/BelongController.kt
@@ -2,12 +2,15 @@ package com.dclass.backend.ui.api
 
 import com.dclass.backend.application.BelongService
 import com.dclass.backend.application.dto.SwitchDepartmentResponse
+import com.dclass.backend.application.dto.UpdateDepartmentRequest
 import com.dclass.backend.domain.user.User
 import com.dclass.backend.security.LoginUser
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -24,5 +27,16 @@ class BelongController(
         @LoginUser user: User
     ): ResponseEntity<ApiResponses<SwitchDepartmentResponse>> {
         return ResponseEntity.ok(ApiResponses.success(belongService.switchDepartment(user.id)))
+    }
+
+    @Operation(summary = "학과 변경 API", description = "학과를 변경합니다")
+    @ApiResponse(responseCode = "204", description = "학과 변경 성공")
+    @PutMapping("/change-departments")
+    fun changeDepartments(
+        @RequestBody @Valid request: UpdateDepartmentRequest,
+        @LoginUser user: User
+    ): ResponseEntity<Unit> {
+        belongService.editDepartments(user.id, request)
+        return ResponseEntity.noContent().build()
     }
 }
