e8c5f2d178d4522cafc00d36c721ac4c8abda910
Woongbae Yun <devbelly@naver.com>
2024-03-18 10:58:43 +0900

    feat : findByDepartmentId 기능을 추가한다

--- backend/src/main/kotlin/com/dclass/backend/domain/community/CommunityRepository.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/community/CommunityRepository.kt
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface CommunityRepository : JpaRepository<Community, Long> {
     fun findByDepartmentIdIn(departmentIds: List<Long>): List<Community>
+    fun findByDepartmentId(departmentId: Long): List<Community>
 }
