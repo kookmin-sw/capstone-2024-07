7408f368924c8bcaf7f988bb53a15541f6e2ef0e
Woongbae Yun <devbelly@naver.com>
2024-03-19 20:51:12 +0900

    feat : NOT_FOUND_USER 예외를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/exception/user/UserExceptionType.kt
+++ backend/src/main/kotlin/com/dclass/backend/exception/user/UserExceptionType.kt
@@ -12,6 +12,7 @@ enum class UserExceptionType(
     INVALID_PASSWORD_ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "C01", "올바르지 않은 비밀번호 입니다."),
     INVALID_USER_INFORMATION(HttpStatus.UNAUTHORIZED, "C02", "사용자 정보가 일치하지 않습니다."),
 
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "C40", "존재하지 않는 사용자입니다."),
     ;
 
     override fun httpStatus(): HttpStatus {
