530f328d7d13911408ec7ec0c4cc68c83da4d94d
Woongbae Yun <devbelly@naver.com>
2024-03-19 20:29:05 +0900

    refactor : NOT_FOUND_COMMUNITY의 코드를 C01로 변경한다

--- backend/src/main/kotlin/com/dclass/backend/exception/community/CommunityExceptionType.kt
+++ backend/src/main/kotlin/com/dclass/backend/exception/community/CommunityExceptionType.kt
@@ -1,23 +1,22 @@
 package com.dclass.backend.exception.community
 
 import com.dclass.backend.exception.common.BaseExceptionType
-import okhttp3.internal.http.HTTP_NOT_FOUND
 import org.springframework.http.HttpStatus
 
 enum class CommunityExceptionType(
     private val httpStatus: HttpStatus,
-    private val code: Int,
+    private val code: String,
     private val errorMessage: String
 ) : BaseExceptionType {
 
-    NOT_FOUND_COMMUNITY(HttpStatus.NOT_FOUND, HTTP_NOT_FOUND, "존재하지 않는 커뮤니티입니다."),
+    NOT_FOUND_COMMUNITY(HttpStatus.NOT_FOUND, "C01", "존재하지 않는 커뮤니티입니다."),
     ;
 
     override fun httpStatus(): HttpStatus {
         return httpStatus
     }
 
-    override fun code(): Int {
+    override fun code(): String {
         return code
     }
 
