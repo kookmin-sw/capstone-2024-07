50758fd07623aac85a83edf4f5d276d7458b50f4
Woongbae Yun <devbelly@naver.com>
2024-03-20 12:09:54 +0900

    feat : findByDepartmentIdAndTitle 메서드를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/domain/community/CommunityRepository.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/community/CommunityRepository.kt
@@ -4,9 +4,6 @@ import com.dclass.backend.exception.community.CommunityException
 import com.dclass.backend.exception.community.CommunityExceptionType.NOT_FOUND_COMMUNITY
 import org.springframework.data.jpa.repository.JpaRepository
 
-fun CommunityRepository.getByTitleOrThrow(title: String): Community {
-    return findByTitle(title) ?: throw CommunityException(NOT_FOUND_COMMUNITY)
-}
 
 fun CommunityRepository.getByIdOrThrow(id: Long): Community {
     return findById(id).orElseThrow { CommunityException(NOT_FOUND_COMMUNITY) }
@@ -16,4 +13,5 @@ interface CommunityRepository : JpaRepository<Community, Long> {
     fun findByDepartmentIdIn(departmentIds: List<Long>): List<Community>
     fun findByDepartmentId(departmentId: Long): List<Community>
     fun findByTitle(title: String): Community?
+    fun findByDepartmentIdAndTitle(departmentId: Long, title: String): Community?
 }
