2f10201838334b4254103468494f6c5201c1131a
Woongbae Yun <devbelly@naver.com>
2024-03-20 15:15:38 +0900

    feat : 스크랩 서비스를 추가한다

---
+++ backend/src/main/kotlin/com/dclass/backend/application/ScrapService.kt
@@ -0,0 +1,38 @@
+package com.dclass.backend.application
+
+import com.dclass.backend.application.dto.PostResponse
+import com.dclass.backend.domain.post.PostRepository
+import com.dclass.backend.domain.scrap.Scrap
+import com.dclass.backend.domain.scrap.ScrapRepository
+import com.dclass.backend.exception.scrap.ScrapException
+import com.dclass.backend.exception.scrap.ScrapExceptionType.NOT_FOUND_SCRAP
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+@Service
+class ScrapService(
+    private val scrapRepository: ScrapRepository,
+    private val postRepository: PostRepository,
+    private val validator: ScrapValidator
+) {
+
+    fun create(userId: Long, postId: Long) {
+        val post = validator.validateScrapPost(userId, postId)
+        post.increaseScrapCount(1)
+
+        scrapRepository.save(Scrap(userId, postId))
+    }
+
+    fun delete(userId: Long, postId: Long) {
+        val scrap = scrapRepository.findByIdAnAndPostId(userId, postId) ?: throw ScrapException(
+            NOT_FOUND_SCRAP
+        )
+
+        scrapRepository.delete(scrap)
+    }
+
+    fun getAll(userId: Long): List<PostResponse> {
+        return postRepository.findScrapPostByUserId(userId)
+    }
+}
