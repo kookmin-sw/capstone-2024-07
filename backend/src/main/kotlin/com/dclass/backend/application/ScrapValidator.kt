232c87ed69899c811f923ec10de366526942ad59
Woongbae Yun <devbelly@naver.com>
2024-03-20 15:15:15 +0900

    feat : 현재 속해있는 학과 게시글만 스크랩할 수 있도록 Validator를 추가한다

---
+++ backend/src/main/kotlin/com/dclass/backend/application/ScrapValidator.kt
@@ -0,0 +1,39 @@
+package com.dclass.backend.application
+
+import com.dclass.backend.domain.belong.BelongRepository
+import com.dclass.backend.domain.belong.getOrThrow
+import com.dclass.backend.domain.community.CommunityRepository
+import com.dclass.backend.domain.community.getByIdOrThrow
+import com.dclass.backend.domain.post.Post
+import com.dclass.backend.domain.post.PostRepository
+import com.dclass.backend.domain.post.getByIdOrThrow
+import com.dclass.backend.domain.scrap.ScrapRepository
+import com.dclass.backend.exception.scrap.ScrapException
+import com.dclass.backend.exception.scrap.ScrapExceptionType.ALREADY_SCRAP_POST
+import com.dclass.backend.exception.scrap.ScrapExceptionType.PERMISSION_DENIED
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+@Service
+class ScrapValidator(
+    private val scrapRepository: ScrapRepository,
+    private val belongRepository: BelongRepository,
+    private val postRepository: PostRepository,
+    private val communityRepository: CommunityRepository
+) {
+    fun validateScrapPost(userId: Long, postId: Long): Post {
+        if (scrapRepository.existsByUserIdAndPostId(userId, postId)) {
+            throw ScrapException(ALREADY_SCRAP_POST)
+        }
+        val belong = belongRepository.getOrThrow(userId)
+        val post = postRepository.getByIdOrThrow(postId)
+        val community = communityRepository.getByIdOrThrow(post.communityId)
+
+        if (belong.contain(community.departmentId)) {
+            throw ScrapException(PERMISSION_DENIED)
+        }
+        return post
+    }
+}
+
