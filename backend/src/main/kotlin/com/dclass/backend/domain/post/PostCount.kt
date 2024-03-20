5f212a00847bcf6c96a66c944370d9e8697e7cb7
Woongbae Yun <devbelly@naver.com>
2024-03-20 15:08:27 +0900

    feat : 스크랩 프로퍼티를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/domain/post/PostCount.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/post/PostCount.kt
@@ -5,11 +5,15 @@ import jakarta.persistence.Embeddable
 @Embeddable
 class PostCount(
     val commentReplyCount: Int = 0,
-    val likeCount: Int = 0
+    val likeCount: Int = 0,
+    val scrapCount: Int = 0,
 ) {
     fun increaseCommentReplyCount(cnt: Int) =
-        PostCount(commentReplyCount + cnt,likeCount)
+        PostCount(commentReplyCount + cnt, likeCount, scrapCount)
 
     fun syncLikeCount(cnt: Int) =
-        PostCount(commentReplyCount,cnt)
+        PostCount(commentReplyCount, cnt, scrapCount)
+
+    fun increaseScrapCount(cnt: Int) =
+        PostCount(commentReplyCount, likeCount, scrapCount + cnt)
 }
