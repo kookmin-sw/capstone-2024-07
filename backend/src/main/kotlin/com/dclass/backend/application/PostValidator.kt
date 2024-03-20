13804b43a11491cf044a645404e58a9640dd9369
Woongbae Yun <devbelly@naver.com>
2024-03-20 12:09:10 +0900

    refactor : 사용하지 않는 메서드를 제거한다

--- backend/src/main/kotlin/com/dclass/backend/application/PostValidator.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/PostValidator.kt
@@ -4,7 +4,6 @@ import com.dclass.backend.domain.belong.BelongRepository
 import com.dclass.backend.domain.belong.getOrThrow
 import com.dclass.backend.domain.community.CommunityRepository
 import com.dclass.backend.domain.community.getByIdOrThrow
-import com.dclass.backend.domain.community.getByTitleOrThrow
 import com.dclass.backend.domain.post.PostRepository
 import com.dclass.backend.domain.post.getByIdOrThrow
 import com.dclass.backend.exception.post.PostException
@@ -19,15 +18,6 @@ class PostValidator(
     private val communityRepository: CommunityRepository,
     private val postRepository: PostRepository
 ) {
-    fun validateCreatePost(userId: Long, communityTitle: String) {
-        val belong = belongRepository.getOrThrow(userId)
-        val community = communityRepository.getByTitleOrThrow(communityTitle)
-
-        if (!belong.contain(community.departmentId)) {
-            throw PostException(FORBIDDEN_POST)
-        }
-    }
-
     fun validate(userId: Long, postId: Long) {
         val belong = belongRepository.getOrThrow(userId)
         val post = postRepository.getByIdOrThrow(postId)
