177981304f45429a9c2aca71af8c3b74ce4c332d
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-21 12:25:30 +0900

    feat: University Entity sql soft deletion을 제거한다

--- backend/src/main/kotlin/com/dclass/backend/domain/user/University.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/user/University.kt
@@ -3,11 +3,7 @@ package com.dclass.backend.domain.user
 import com.dclass.support.domain.BaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import org.hibernate.annotations.SQLDelete
-import org.hibernate.annotations.SQLRestriction
 
-@SQLDelete(sql = "update university set deleted = true where id = ?")
-@SQLRestriction("deleted = false")
 @Entity
 class University(
 
@@ -24,9 +20,4 @@ class University(
     val logo: String = "",
 
     id: Long = 0L
-) : BaseEntity(id) {
-
-    @Column(nullable = false)
-    var deleted: Boolean = false
-        protected set
-}
+) : BaseEntity(id)
