56ea8e25f0e716b62e585a37100155381953fc5e
Woongbae Yun <devbelly@naver.com>
2024-03-24 13:11:31 +0900

    fix : 대댓글이 없을 때 jpql 로직을 변경한다

--- backend/src/main/kotlin/com/dclass/backend/domain/reply/ReplyRepositorySupport.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/reply/ReplyRepositorySupport.kt
@@ -2,7 +2,9 @@ package com.dclass.backend.domain.reply
 
 import com.dclass.backend.application.dto.ReplyWithUserResponse
 import com.dclass.backend.domain.user.User
+import com.linecorp.kotlinjdsl.dsl.jpql.Jpql
 import com.linecorp.kotlinjdsl.dsl.jpql.jpql
+import com.linecorp.kotlinjdsl.querymodel.jpql.predicate.Predicatable
 import com.linecorp.kotlinjdsl.render.jpql.JpqlRenderContext
 import com.linecorp.kotlinjdsl.support.spring.data.jpa.extension.createQuery
 import jakarta.persistence.EntityManager
@@ -25,10 +27,14 @@ class ReplyRepositoryImpl(
                 entity(Reply::class),
                 join(User::class).on(path(Reply::userId).eq(path(User::id)))
             ).where(
-                path(Reply::commentId).`in`(commentIds)
+                replyExist(commentIds)
             )
         }
 
         return em.createQuery(query, context).resultList
+    }
+
+    private fun Jpql.replyExist(request: List<Long>): Predicatable? {
+        return if (request.isNotEmpty()) path(Reply::commentId).`in`(request) else null
     }
 }
