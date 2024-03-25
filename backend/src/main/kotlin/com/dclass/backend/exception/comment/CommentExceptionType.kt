50e2d5aa64414250287239d6447c1e960200ccac
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-25 16:33:01 +0900

    feat: comment custom exception을 추가한다

--- backend/src/main/kotlin/com/dclass/backend/exception/comment/CommentExceptionType.kt
+++ backend/src/main/kotlin/com/dclass/backend/exception/comment/CommentExceptionType.kt
@@ -9,6 +9,7 @@ enum class CommentExceptionType(
     private val errorMessage: String
 ) : BaseExceptionType {
 
+    FORBIDDEN_COMMENT(HttpStatus.FORBIDDEN, "C00", "댓글에 대한 권한이 없습니다."),
     NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, "C01", "해당 댓글을 찾을 수 없습니다."),
     SELF_LIKE(HttpStatus.BAD_REQUEST, "C02", "본인이 작성한 댓글에는 좋아요를 누를 수 없습니다.");
 
