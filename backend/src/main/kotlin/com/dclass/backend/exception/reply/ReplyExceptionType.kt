dfb54dfd4a1f91f442e07ed83a05de3a698ef8b6
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-25 16:34:33 +0900

    feat: reply custom exception을 추가한다

--- backend/src/main/kotlin/com/dclass/backend/exception/reply/ReplyExceptionType.kt
+++ backend/src/main/kotlin/com/dclass/backend/exception/reply/ReplyExceptionType.kt
@@ -9,6 +9,7 @@ enum class ReplyExceptionType(
     private val errorMessage: String
 ) : BaseExceptionType {
 
+    FORBIDDEN_REPLY(HttpStatus.FORBIDDEN, "R00", "대댓글에 대한 권한이 없습니다."),
     NOT_FOUND_REPLY(HttpStatus.NOT_FOUND, "R01", "존재하지 않는 댓글입니다."),
     SELF_LIKE(HttpStatus.BAD_REQUEST, "R02", "본인이 작성한 대댓글에는 좋아요를 누를 수 없습니다.")
     ;
