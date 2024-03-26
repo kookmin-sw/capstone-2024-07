8311c636380a58583bd1eb66b6483a1a7d7b9473
jia5232 <jia5232@naver.com>
2024-03-26 09:15:31 +0900

    feat: 학과 선택 selectbox ui 및 provider를 추가한다.

--- frontend/lib/member/provider/selected_major_provider.dart
+++ frontend/lib/member/provider/selected_major_provider.dart
@@ -1,3 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final selectedMajorProvider = StateProvider<String>((ref) => "");
+// final selectedMajorProvider = StateProvider<String>((ref) => "");
+final selectedMajorProvider = StateProvider<String>((ref) => "컴퓨터공학과");
