aaa79d05c46aeace4ef06a4c4f621fdfc9056410
Woongbae Yun <devbelly@naver.com>
2024-03-26 15:42:26 +0900

    feat : Token Resolver를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/config/AuthenticationConfig.kt
+++ backend/src/main/kotlin/com/dclass/backend/config/AuthenticationConfig.kt
@@ -1,5 +1,6 @@
 package com.dclass.backend.config
 
+import com.dclass.backend.security.AuthTokenResolver
 import com.dclass.backend.security.LoginUserResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
@@ -8,9 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class AuthenticationConfig(
     private val loginUserResolver: LoginUserResolver,
-
-) : WebMvcConfigurer{
+    private val authTokenResolver: AuthTokenResolver
+) : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(loginUserResolver)
+        resolvers.add(authTokenResolver)
     }
 }
