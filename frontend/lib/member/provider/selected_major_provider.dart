426707166fe2423834f77442b200c3f12dcbd8d2
jia5232 <jia5232@naver.com>
2024-03-26 10:06:54 +0900

    feat: 학과 변경기능 ui를 api와 연결

--- frontend/lib/member/provider/selected_major_provider.dart
+++ frontend/lib/member/provider/selected_major_provider.dart
@@ -1,4 +1,3 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// final selectedMajorProvider = StateProvider<String>((ref) => "");
-final selectedMajorProvider = StateProvider<String>((ref) => "컴퓨터공학과");
+final selectedMajorProvider = StateProvider<String>((ref) => "");
