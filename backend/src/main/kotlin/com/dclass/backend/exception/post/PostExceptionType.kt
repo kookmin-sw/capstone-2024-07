b9e452ca3fd99fa4cc8e6de6277e29f78193666a
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-03-27 15:50:35 +0900

    feat: post custom exception 을 추가한다

--- backend/src/main/kotlin/com/dclass/backend/exception/post/PostExceptionType.kt
+++ backend/src/main/kotlin/com/dclass/backend/exception/post/PostExceptionType.kt
@@ -10,6 +10,7 @@ enum class PostExceptionType(
 ) : BaseExceptionType {
 
     NOT_FOUND_POST(HttpStatus.NOT_FOUND, "P01", "존재하지 않는 게시글입니다"),
+    SELF_LIKE(HttpStatus.BAD_REQUEST, "P02", "본인 게시글에 좋아요를 누를 수 없습니다"),
     FORBIDDEN_POST(HttpStatus.FORBIDDEN, "P43", "해당 커뮤니티에 게시글을 작성할 수 없습니다"),
     ;
 
