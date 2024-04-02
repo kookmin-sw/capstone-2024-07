daba4b6fd020ae5b8a16e11462938c810cab3ebe
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2024-04-02 17:36:42 +0900

    refactor: dto를 반환하도록 수정한다

--- backend/src/main/kotlin/com/dclass/backend/application/CommentValidator.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/CommentValidator.kt
@@ -1,12 +1,12 @@
 package com.dclass.backend.application
 
+import com.dclass.backend.application.dto.CommentValidatorDto
 import com.dclass.backend.domain.belong.BelongRepository
 import com.dclass.backend.domain.belong.getOrThrow
 import com.dclass.backend.domain.comment.CommentRepository
 import com.dclass.backend.domain.comment.getByIdOrThrow
 import com.dclass.backend.domain.community.CommunityRepository
 import com.dclass.backend.domain.community.getByIdOrThrow
-import com.dclass.backend.domain.post.Post
 import com.dclass.backend.domain.post.PostRepository
 import com.dclass.backend.domain.post.getByIdOrThrow
 import com.dclass.backend.exception.comment.CommentException
@@ -22,7 +22,7 @@ class CommentValidator(
     private val communityRepository: CommunityRepository,
     private val commentRepository: CommentRepository,
 ) {
-    fun validateCreateComment(userId: Long, postId: Long): Post {
+    fun validateCreateComment(userId: Long, postId: Long): CommentValidatorDto {
         return validateCommon(userId, postId)
     }
 
@@ -32,7 +32,7 @@ class CommentValidator(
 
     }
 
-    private fun validateCommon(userId: Long, postId: Long): Post {
+    private fun validateCommon(userId: Long, postId: Long): CommentValidatorDto {
         val belong = belongRepository.getOrThrow(userId)
         val post = postRepository.getByIdOrThrow(postId)
         val community =
@@ -41,6 +41,6 @@ class CommentValidator(
             throw CommentException(FORBIDDEN_COMMENT)
         }
 
-        return post
+        return CommentValidatorDto(post, community.title)
     }
 }
