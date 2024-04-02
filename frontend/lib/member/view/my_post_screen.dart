824447e0c8e1c8f9906a966bafcecd1f1f7b36d4
jia5232 <jia5232@naver.com>
2024-04-02 16:05:21 +0900

    feat: 내가 쓴 글 ui에 api 연결

--- frontend/lib/member/view/my_post_screen.dart
+++ frontend/lib/member/view/my_post_screen.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frontend/member/provider/mypage/my_post_state_notifier_provider.dart';
 
+import '../../board/component/board_card.dart';
+import '../../board/model/msg_board_response_model.dart';
+import '../../board/view/msg_board_screen.dart';
 import '../../common/const/colors.dart';
+import '../../common/model/cursor_pagination_model.dart';
 
-class MyPostScreen extends StatefulWidget {
+class MyPostScreen extends ConsumerStatefulWidget {
   const MyPostScreen({super.key});
 
   @override
-  State<MyPostScreen> createState() => _MyPostScreenState();
+  ConsumerState<MyPostScreen> createState() => _MyPostScreenState();
 }
 
-class _MyPostScreenState extends State<MyPostScreen> {
+class _MyPostScreenState extends ConsumerState<MyPostScreen> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    if (controller.offset > controller.position.maxScrollExtent - 150) {
+      ref.read(myPostStateNotifierProvider.notifier).paginate(
+            fetchMore: true,
+          );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,13 +39,16 @@ class _MyPostScreenState extends State<MyPostScreen> {
         child: Column(
           children: [
             _renderTop(),
+            Expanded(
+              child: _renderMyPostList(),
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget _renderTop(){
+  Widget _renderTop() {
     return Container(
       child: Row(
         children: [
@@ -44,6 +69,68 @@ class _MyPostScreenState extends State<MyPostScreen> {
           ),
         ],
       ),
+    );
+  }
+
+  Widget _renderMyPostList() {
+    final data = ref.watch(myPostStateNotifierProvider);
+
+    if (data is CursorPaginationModelLoading) {
+      return const Center(
+        child: CircularProgressIndicator(
+          color: PRIMARY_COLOR,
+        ),
+      );
+    }
+
+    if (data is CursorPaginationModelError) {
+      return const Center(
+        child: Text("데이터를 불러올 수 없습니다."),
+      );
+    }
+
+    final cp = data as CursorPaginationModel;
+
+    return ListView.separated(
+      controller: controller,
+      itemCount: cp.data.length + 1,
+      itemBuilder: (_, index) {
+        if (index == cp.data.length) {
+          return Center(
+            child: cp is CursorPaginationModelFetchingMore
+                ? const CircularProgressIndicator(
+                    color: PRIMARY_COLOR,
+                  )
+                : const Text(
+                    'Copyright 2024. Decl Team all rights reserved.\n',
+                    style: TextStyle(
+                      color: BODY_TEXT_COLOR,
+                      fontSize: 12.0,
+                    ),
+                  ),
+          );
+        }
+
+        final MsgBoardResponseModel pItem = cp.data[index];
+
+        return GestureDetector(
+          child: BoardCard.fromModel(msgBoardResponseModel: pItem),
+          onTap: () async {
+            // 상세페이지
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => MsgBoardScreen(
+                        board: pItem,
+                      ),
+                  fullscreenDialog: true),
+            );
+          },
+        );
+      },
+      separatorBuilder: (_, index) {
+        return const SizedBox(height: 1.0);
+      },
     );
   }
 }
