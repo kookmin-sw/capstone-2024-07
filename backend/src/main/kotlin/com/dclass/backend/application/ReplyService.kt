7f14407ab430784d015921ff6c13928e57ea5f79
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-04-02 17:46:39 +0900

    feat: 대댓글 작성시 게시글 작성자에게 알림을 전송하도록 이벤트를 발행한다

--- backend/src/main/kotlin/com/dclass/backend/application/ReplyService.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/ReplyService.kt
@@ -1,11 +1,14 @@
 package com.dclass.backend.application
 
 import com.dclass.backend.application.dto.*
+import com.dclass.backend.domain.notification.NotificationCommentEvent
+import com.dclass.backend.domain.notification.NotificationType
 import com.dclass.backend.domain.post.PostRepository
 import com.dclass.backend.domain.post.getByIdOrThrow
 import com.dclass.backend.domain.reply.ReplyRepository
 import com.dclass.backend.domain.reply.getByIdAndUserIdOrThrow
 import com.dclass.backend.domain.reply.getByIdOrThrow
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -15,13 +18,25 @@ class ReplyService(
     private val replyRepository: ReplyRepository,
     private val replyValidator: ReplyValidator,
     private val postRepository: PostRepository,
+    private val eventPublisher: ApplicationEventPublisher
 ) {
     fun create(userId: Long, request: CreateReplyRequest): ReplyResponse {
-        val comment = replyValidator.validateCreateReply(userId, request.commentId)
-        val post = postRepository.getByIdOrThrow(comment.postId)
-        post.increaseCommentReplyCount(1)
-        return replyRepository.save(request.toEntity(userId))
-            .let(::ReplyResponse)
+        val replyValidatorDto = replyValidator.validateCreateReply(userId, request.commentId)
+        val postUserId = postRepository.getByIdOrThrow(replyValidatorDto.postId).userId
+        val reply = replyRepository.save(request.toEntity(userId))
+        eventPublisher.publishEvent(
+            NotificationCommentEvent(
+                postUserId,
+                replyValidatorDto.postId,
+                request.commentId,
+                request.content,
+                replyValidatorDto.communityTitle,
+                NotificationType.COMMENT
+            )
+        )
+
+        return ReplyResponse(reply)
+
     }
 
     fun update(userId: Long, request: UpdateReplyRequest) {
