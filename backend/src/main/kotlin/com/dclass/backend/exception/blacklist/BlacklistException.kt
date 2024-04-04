2c2e893ca84e3fadff53217c6f015fda0834d05e
Woongbae Yun <devbelly@naver.com>
2024-04-04 12:19:47 +0900

    feat : blacklist 관련 예외를 추가한다

---
+++ backend/src/main/kotlin/com/dclass/backend/exception/blacklist/BlacklistException.kt
@@ -0,0 +1,11 @@
+package com.dclass.backend.exception.blacklist
+
+import com.dclass.backend.exception.common.BaseException
+
+class BlacklistException(
+    private val exceptionType: BlacklistExceptionType
+) : BaseException() {
+    override fun exceptionType(): BlacklistExceptionType {
+        return exceptionType
+    }
+}
