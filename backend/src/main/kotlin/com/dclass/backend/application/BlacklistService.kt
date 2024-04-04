8efacae46aded0046788f89f10d3afabc82e81c3
Woongbae Yun <devbelly@naver.com>
2024-04-04 12:21:19 +0900

    fix : 발생하는 예외를 명확하게 수정한다

--- backend/src/main/kotlin/com/dclass/backend/application/BlacklistService.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/BlacklistService.kt
@@ -3,6 +3,9 @@ package com.dclass.backend.application
 import com.dclass.backend.application.dto.LoginUserResponse
 import com.dclass.backend.domain.blacklist.Blacklist
 import com.dclass.backend.domain.blacklist.BlacklistRepository
+import com.dclass.backend.exception.blacklist.BlacklistException
+import com.dclass.backend.exception.blacklist.BlacklistExceptionType
+import com.dclass.backend.exception.blacklist.BlacklistExceptionType.ALREADY_LOGOUT
 import com.dclass.backend.exception.token.TokenException
 import com.dclass.backend.exception.token.TokenExceptionType.TOKEN_NOT_FOUND_IN_BLACKLIST
 import com.dclass.backend.security.JwtTokenProvider
@@ -19,7 +22,7 @@ class BlacklistService(
     fun reissueToken(refreshToken: String): LoginUserResponse {
         jwtTokenProvider.isValidToken(refreshToken)
         blacklistRepository.findByInvalidRefreshToken(refreshToken)
-            ?.let { throw TokenException(TOKEN_NOT_FOUND_IN_BLACKLIST) }
+            ?.let { throw BlacklistException(ALREADY_LOGOUT) }
             ?: blacklistRepository.save(Blacklist(refreshToken))
 
         val email = jwtTokenProvider.getSubject(refreshToken)
