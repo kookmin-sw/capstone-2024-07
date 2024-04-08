110802e4c6c261efdc35c080fb5921cc98cfd63d
Woongbae Yun <devbelly@naver.com>
2024-04-08 14:31:17 +0900

    feat : 학과 변경 남은 일수 DTO를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/application/dto/BelongDtos.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/dto/BelongDtos.kt
@@ -1,6 +1,7 @@
 package com.dclass.backend.application.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
+import java.time.Duration
 
 data class SwitchDepartmentResponse(
     @Schema(description = "활성화된 학과 이름", example = "컴퓨터공학과")
@@ -14,3 +15,18 @@ data class UpdateDepartmentRequest(
     @Schema(description = "변경할 부전공의 이름", example = "경영학과")
     val minor: String,
 )
+
+data class RemainDurationResponse(
+    @Schema(description = "남은 학과 변경 일수", example = "10")
+    val remainDays: Long,
+    @Schema(description = "남은 학과 변경 시간", example = "10")
+    val remainHours: Long,
+    @Schema(description = "남은 학과 변경 분", example = "10")
+    val remainMinutes: Long,
+) {
+    constructor(remain: Duration) : this(
+        remain.toDays(),
+        remain.toHours(),
+        remain.toMinutes(),
+    )
+}
