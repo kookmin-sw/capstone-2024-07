e554b5355cc7f301e2f120f86a1d798109385e36
jia5232 <jia5232@naver.com>
2024-04-09 22:42:15 +0900

    feat: 임시 비밀번호 발급 기능

--- frontend/lib/member/view/password_reset_screen.dart
+++ frontend/lib/member/view/password_reset_screen.dart
@@ -1,14 +1,28 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:frontend/common/layout/default_layout.dart';
 
-class PasswordResetScreen extends StatefulWidget {
+import '../../common/component/notice_popup_dialog.dart';
+import '../../common/const/colors.dart';
+import '../../common/const/data.dart';
+import '../../common/provider/dio_provider.dart';
+import '../component/custom_text_form_field.dart';
+
+class PasswordResetScreen extends ConsumerStatefulWidget {
   const PasswordResetScreen({super.key});
 
   @override
-  State<PasswordResetScreen> createState() => _PasswordResetScreenState();
+  ConsumerState<PasswordResetScreen> createState() => _PasswordResetScreenState();
 }
 
-class _PasswordResetScreenState extends State<PasswordResetScreen> {
+class _PasswordResetScreenState extends ConsumerState<PasswordResetScreen> {
+  bool isNameNull = true;
+  bool isEmailNull = true;
+
+  String name = "";
+  String email = "";
+
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
@@ -20,14 +34,188 @@ class _PasswordResetScreenState extends State<PasswordResetScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Center(
-                  child: Text('비밀번호 찾기 페이지'),
-                ),
+                _renderTop(),
+                const SizedBox(height: 20.0),
+                _renderNameField(),
+                _renderEmailField(),
+                const SizedBox(height: 20.0),
+                _renderButton(),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _renderTop() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(
+              Icons.chevron_left,
+            ),
+          ),
+          const SizedBox(
+            width: 86,
+          ),
+          const Text(
+            '비밀번호 초기화',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _renderNameField() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('이름'),
+          const SizedBox(height: 6.0),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: CustomTextFormField(
+              isInputEnabled: true,
+              hintText: '이름(본명)을 입력해주세요.',
+              onChanged: (String value) {
+                name = value;
+                setState(() {
+                  isNameNull = name == "" ? true : false;
+                });
+              },
+            ),
+          ),
+          if (isNameNull)
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                '이름은 빈칸일 수 없습니다.',
+                style: TextStyle(
+                  fontSize: 12.0,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _renderEmailField() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('이메일'),
+          const SizedBox(height: 6.0),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: CustomTextFormField(
+              isInputEnabled: true,
+              hintText: '이메일을 입력해주세요.',
+              onChanged: (String value) {
+                email = value;
+                setState(() {
+                  isEmailNull = email == "" ? true : false;
+                });
+              },
+            ),
+          ),
+          if (isEmailNull)
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                '이메일은 빈칸일 수 없습니다.',
+                style: TextStyle(
+                  fontSize: 12.0,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _renderButton() {
+    final dio = ref.watch(dioProvider);
+
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: PRIMARY_COLOR,
+        minimumSize: Size(
+          MediaQuery.of(context).size.width,
+          50.0,
+        ),
+      ),
+      onPressed: () async {
+        if (!isEmailNull &&
+            !isNameNull) {
+          try {
+            final resp = await dio.post(
+              'http://$ip/api/users/reset-password',
+              data: {
+                'name': name,
+                'email': email,
+              },
+            );
+            if (resp.statusCode == 204) {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return NoticePopupDialog(
+                    message: "새로운 임시 비밀번호를 이메일로 전송했습니다.",
+                    buttonText: "닫기",
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                    },
+                  );
+                },
+              );
+            }
+          } on DioException catch (e) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return NoticePopupDialog(
+                  message: e.response?.data["message"] ?? "에러가 발생했습니다.",
+                  buttonText: "닫기",
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                );
+              },
+            );
+          } catch (e){
+            showDialog(
+              context: context,
+              builder: (context) {
+                return NoticePopupDialog(
+                  message: "에러가 발생했습니다.",
+                  buttonText: "닫기",
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                );
+              },
+            );
+          }
+        }
+      },
+      child: Text('임시 비밀번호 발급'),
     );
   }
 }
