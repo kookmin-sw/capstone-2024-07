8ec89e8a52814648a57bc6e5f8e67b2c899a4e48
Woongbae Yun <devbelly@naver.com>
2024-04-09 23:49:12 +0900

    fix : 예외 메세지를 수정한다

--- backend/src/main/kotlin/com/dclass/backend/exception/belong/BelongExceptionType.kt
+++ backend/src/main/kotlin/com/dclass/backend/exception/belong/BelongExceptionType.kt
@@ -11,7 +11,7 @@ enum class BelongExceptionType(
 
     NOT_FOUND_BELONG(HttpStatus.NOT_FOUND, "B01", "해당 소속을 찾을 수 없습니다."),
     MAX_BELONG(HttpStatus.BAD_REQUEST, "B02", "소속은 최대 2개까지만 설정할 수 있습니다."),
-    CHANGE_INTERVAL_VIOLATION(HttpStatus.BAD_REQUEST, "B03", "학과 변경은 최대 90일마다 가능합니다."),
+    CHANGE_INTERVAL_VIOLATION(HttpStatus.BAD_REQUEST, "B03", "학과 변경은 90일마다 가능합니다."),
     ;
 
     override fun httpStatus(): HttpStatus {
