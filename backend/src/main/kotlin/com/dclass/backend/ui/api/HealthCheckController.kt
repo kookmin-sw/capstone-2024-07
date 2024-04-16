0bdf7943e148891c646a1fa6bc188c587780af0d
Woongbae Yun <devbelly@naver.com>
2024-04-16 16:11:40 +0900

    hotfix : healthCheck 컨트롤러를 추가한다

---
+++ backend/src/main/kotlin/com/dclass/backend/ui/api/HealthCheckController.kt
@@ -0,0 +1,14 @@
+package com.dclass.backend.ui.api
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/")
+@RestController
+class HealthCheckController {
+    @RequestMapping
+    fun healthCheck(): ResponseEntity<String> {
+        return ResponseEntity.ok("Service is up and running!")
+    }
+}
