a331fe9f9dababe6cef1b8596ceaee9265329a22
hongbuly <hongbuly@naver.com>
2024-04-16 14:43:10 +0900

    feat : add python api provider and response model

---
+++ frontend/lib/board/model/python_response_model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'python_response_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PythonResponseModel _$PythonResponseModelFromJson(Map<String, dynamic> json) =>
+    PythonResponseModel(
+      prafanity: json['prafanity'] as bool,
+    );
+
+Map<String, dynamic> _$PythonResponseModelToJson(
+        PythonResponseModel instance) =>
+    <String, dynamic>{
+      'prafanity': instance.prafanity,
+    };
