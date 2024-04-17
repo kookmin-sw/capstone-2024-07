6b2afa6faec163edd4103f6b84f610c7004395fd
Woongbae Yun <devbelly@naver.com>
2024-04-17 12:58:44 +0900

    fix : health-check 경로를 변경한다

--- backend/src/main/kotlin/com/dclass/backend/ui/api/HealthCheckController.kt
+++ backend/src/main/kotlin/com/dclass/backend/ui/api/HealthCheckController.kt
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/")
+@RequestMapping("/api/health-check")
 @RestController
 class HealthCheckController {
     @RequestMapping
