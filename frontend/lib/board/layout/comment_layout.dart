1accb63ecd64bdd8e8e2a936f8f1ce0dab1f85a1
hongbuly <hongbuly@naver.com>
2024-04-19 22:05:21 +0900

    fix : 시간 포맷 잘못된거 수정

--- frontend/lib/board/layout/comment_layout.dart
+++ frontend/lib/board/layout/comment_layout.dart
@@ -43,14 +43,10 @@ class _CommentState extends ConsumerState<Comment>
     String nowTime = dt.replaceRange(0, 11, ""); //20:09:14.322471
     nowTime = nowTime.replaceRange(9, nowTime.length, ""); //20:09:14
 
-    debugPrint("nowDate : $nowDate, nowTime : $nowTime");
-
     time = time.replaceAll('T', " ");
     String uploadDate = time.replaceRange(11, time.length, "");
     String uploadTime = time.replaceRange(0, 11, "");
     uploadTime = uploadTime.replaceRange(9, uploadTime.length, "");
-
-    debugPrint("uploadDate : $uploadDate, uploadTime : $uploadTime");
 
     if (nowDate == uploadDate) {
       if (nowTime.replaceRange(2, nowTime.length, "") ==
@@ -60,8 +56,6 @@ class _CommentState extends ConsumerState<Comment>
         nowTmp = nowTmp.replaceRange(2, nowTmp.length, "");
         String uploadTmp = uploadTime.replaceRange(0, 3, "");
         uploadTmp = uploadTmp.replaceRange(2, uploadTmp.length, "");
-
-        debugPrint("nowTmp : $nowTmp, uploadTmp : $uploadTmp");
 
         if (int.parse(nowTmp) - int.parse(uploadTmp) == 0) {
           return "방금전";
@@ -77,9 +71,7 @@ class _CommentState extends ConsumerState<Comment>
         String uploadTmp = uploadTime.replaceRange(0, 3, "");
         uploadTmp = uploadTmp.replaceRange(2, uploadTmp.length, "");
 
-        debugPrint("nowTmp : $nowTmp, uploadTmp : $uploadTmp");
-
-        return "${int.parse(nowTmp) - int.parse(uploadTmp)}분전";
+        return "${int.parse(uploadTmp) - int.parse(nowTmp)}분전";
       }
     }
 
