48362b978d74f56669856ce408fc71b4d1cc8cc2
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-04-22 20:28:15 +0900

    feat: spring scheduler를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/dclass/backend/BackendApplication.kt
@@ -3,11 +3,13 @@ package com.dclass.backend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableScheduling
 class BackendApplication
 
 fun main(args: Array<String>) {
-	runApplication<BackendApplication>(*args)
+    runApplication<BackendApplication>(*args)
 }
