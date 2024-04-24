c62fd20a678c09b2cfeaff8169ae97ea9d9d5d4b
jia5232 <jia5232@naver.com>
2024-04-24 09:00:13 +0900

    feat: 검색 스크린 추가

---
+++ frontend/lib/board/view/search_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Center(
+              child: Text("search screen"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
