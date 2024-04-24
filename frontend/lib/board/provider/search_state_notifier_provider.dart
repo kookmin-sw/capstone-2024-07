2166c92b2554c30c1bb934b21e1c3ef3508f6f47
jia5232 <jia5232@naver.com>
2024-04-24 17:06:02 +0900

    feat: search state notifier 구현

---
+++ frontend/lib/board/provider/search_state_notifier_provider.dart
@@ -0,0 +1,129 @@
+import 'dart:core';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frontend/board/provider/search_repository_provider.dart';
+import 'package:frontend/board/provider/searck_keyword_provider.dart';
+import 'package:frontend/common/model/cursor_pagination_model.dart';
+
+final searchStateNotifierProvider =
+    StateNotifierProvider<SearchStateNotifier, CursorPaginationModelBase>(
+  (ref) {
+    final repository = ref.watch(searchRepositoryProvider);
+    const initialLastPostId = 9223372036854775807;
+    const size = 10;
+    final keyword = ref.watch(searchKeywordProvider.notifier).state;
+
+    final notifier = SearchStateNotifier(
+      repository: repository,
+      lastId: initialLastPostId,
+      size: size,
+      keyword: keyword,
+    );
+    return notifier;
+  },
+);
+
+class SearchStateNotifier extends StateNotifier<CursorPaginationModelBase> {
+  bool _mounted = true;
+  bool _fetchingData = false;
+
+  @override
+  void dispose() {
+    _mounted = false;
+    super.dispose();
+  }
+
+  final SearchRepository repository;
+  int lastId;
+  int size;
+  String keyword;
+
+  SearchStateNotifier({
+    required this.repository,
+    required this.lastId,
+    required this.size,
+    required this.keyword,
+  }) : super(CursorPaginationModelLoading()) {
+    paginate();
+  }
+
+  bool get isMounted => _mounted;
+
+  void updateAndFetch(String newKeyword) {
+    keyword = newKeyword;
+    lastId = 9223372036854775807;
+    paginate(forceRefetch: true);
+  }
+
+  Future<void> paginate({
+    bool fetchMore = false,
+    bool forceRefetch = false,
+  }) async {
+    if (!isMounted) return;
+
+    if (_fetchingData) return;
+    _fetchingData = true;
+
+    try {
+      if (state is CursorPaginationModel && !forceRefetch) {
+        final pState = state as CursorPaginationModel;
+
+        if (!pState.meta.hasMore) {
+          return;
+        }
+      }
+
+      final isLoading = state is CursorPaginationModelLoading;
+      final isRefetching = state is CursorPaginationModelRefetching;
+      final isFetchingMore = state is CursorPaginationModelFetchingMore;
+
+      if (fetchMore && (isLoading || isRefetching || isFetchingMore)) {
+        return;
+      }
+
+      if (fetchMore) {
+        final pState = (state as CursorPaginationModel);
+
+        state = CursorPaginationModelFetchingMore(
+          meta: pState.meta,
+          data: pState.data,
+        );
+        lastId = pState.data.last.id;
+      } else {
+        if (state is CursorPaginationModel && !forceRefetch) {
+          final pState = state as CursorPaginationModel;
+
+          state = CursorPaginationModelRefetching(
+            meta: pState.meta,
+            data: pState.data,
+          );
+        } else {
+          state = CursorPaginationModelLoading();
+        }
+      }
+      final resp = await repository.paginate(lastId, size, keyword);
+
+      if (!isMounted) return;
+
+      if (state is CursorPaginationModelFetchingMore) {
+        final pState = state as CursorPaginationModelFetchingMore;
+        state = resp.copyWith(
+          data: [
+            ...pState.data,
+            ...resp.data,
+          ],
+        );
+      } else {
+        state = resp;
+      }
+    } catch (e) {
+      if (!isMounted) return;
+
+      print(e.runtimeType);
+
+      state = CursorPaginationModelError(message: '데이터를 가져오지 못했습니다');
+    } finally {
+      _fetchingData = false;
+    }
+  }
+}
