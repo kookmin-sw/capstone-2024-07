b4ec45f8e78a51074c8bce2730305b69df80a3c9
Woongbae Yun <devbelly@naver.com>
2024-04-24 11:18:57 +0900

    test : 답글을 달면 댓글 수가 증가하고 알림이 발송된다

--- backend/src/test/kotlin/com/dclass/backend/application/ReplyServiceTest.kt
+++ backend/src/test/kotlin/com/dclass/backend/application/ReplyServiceTest.kt
@@ -5,15 +5,21 @@ import com.dclass.backend.domain.comment.Comment
 import com.dclass.backend.domain.comment.CommentRepository
 import com.dclass.backend.domain.comment.getByIdOrThrow
 import com.dclass.backend.domain.community.CommunityRepository
+import com.dclass.backend.domain.community.findByIdOrThrow
+import com.dclass.backend.domain.notification.NotificationEvent
 import com.dclass.backend.domain.post.PostRepository
+import com.dclass.backend.domain.post.findByIdOrThrow
 import com.dclass.backend.domain.reply.ReplyRepository
 import com.dclass.backend.exception.comment.CommentException
 import com.dclass.backend.exception.comment.CommentExceptionType
+import com.dclass.support.fixtures.comment
+import com.dclass.support.fixtures.community
+import com.dclass.support.fixtures.post
+import com.dclass.support.fixtures.reply
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockk
+import io.mockk.*
 import org.springframework.context.ApplicationEventPublisher
 
 class ReplyServiceTest : BehaviorSpec({
@@ -44,6 +50,51 @@ class ReplyServiceTest : BehaviorSpec({
                 shouldThrow<CommentException> {
                     replyService.create(1L, CreateReplyRequest(1L, "reply content"))
                 }.exceptionType() shouldBe CommentExceptionType.DELETED_COMMENT
+            }
+        }
+    }
+
+    Given("댓글이 존재하는 경우") {
+        val comment = comment()
+        every { commentRepository.getByIdOrThrow(any()) } returns comment
+
+        val post = post()
+        every { postRepository.findByIdOrThrow(any()) } returns post
+        val community = community()
+        every { communityRepository.findByIdOrThrow(any()) } returns community
+
+        justRun { replyValidator.validate(any(), any()) }
+
+        val reply = reply()
+        every { replyRepository.save(any()) } returns reply
+
+        mockkObject(NotificationEvent.Companion) {
+            every { NotificationEvent.replyToPostUser(any(), any(), any(), any()) } returns mockk()
+            every {
+                NotificationEvent.replyToCommentUser(
+                    any(),
+                    any(),
+                    any(),
+                    any()
+                )
+            } returns mockk()
+        }
+
+        justRun { eventPublisher.publishEvent(any<NotificationEvent>()) }
+
+        When("다른 사람이 답글을 달면") {
+            replyService.create(2L, CreateReplyRequest(1L, "reply content"))
+
+            Then("답글이 생성된다") {
+                verify { replyRepository.save(any()) }
+            }
+
+            Then("댓글의 답글 수가 증가한다") {
+                comment.replyCount shouldBe 1
+            }
+
+            Then("답글 알림을 발송한다") {
+                verify { eventPublisher.publishEvent(any<NotificationEvent>()) }
             }
         }
     }
