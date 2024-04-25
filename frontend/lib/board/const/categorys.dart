103ce45a43ef91b9b7adaca4b0690ae5c9edc2e9
hongbuly <hongbuly@naver.com>
2024-04-25 23:10:50 +0900

    feat : 질문 관련 게시판 및 등록/수정페이지 삭제

--- frontend/lib/board/const/categorys.dart
+++ frontend/lib/board/const/categorys.dart
@@ -5,7 +5,6 @@ const List<String> categorysList = [
   "대학원게시판",
   "취준게시판",
   "스터디모집",
-  "질문게시판",
   "홍보게시판",
 ];
 
@@ -16,7 +15,6 @@ Map<String, String> categoryCodesList = {
   "대학원게시판": "GRADUATE",
   "취준게시판": "JOB",
   "스터디모집": "STUDY",
-  "질문게시판": "QUESTION",
   "홍보게시판": "PROMOTION",
 };
 
@@ -26,6 +24,5 @@ Map<String, String> categoryCodesReverseList = {
   "GRADUATE": "대학원게시판",
   "JOB": "취준게시판",
   "STUDY": "스터디모집",
-  "QUESTION": "질문게시판",
   "PROMOTION": "홍보게시판",
 };
