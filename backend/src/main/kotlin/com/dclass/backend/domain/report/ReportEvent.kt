ee953e4f73237abddcd213fff60b76c2c7dabd8d
Woongbae Yun <devbelly@naver.com>
2024-05-03 16:20:31 +0900

    feat : 신고 관련 이벤트 객체를 추가한다

---
+++ backend/src/main/kotlin/com/dclass/backend/domain/report/ReportEvent.kt
@@ -0,0 +1,26 @@
+package com.dclass.backend.domain.report
+
+interface ReportEvent {
+    companion object {
+        fun create(id: Long, type: ReportType): ReportEvent {
+            return when (type) {
+                ReportType.POST -> PostReportedEvent(id)
+                ReportType.COMMENT -> CommentReportedEvent(id)
+                ReportType.REPLY -> ReplyReportedEvent(id)
+            }
+        }
+    }
+}
+
+class PostReportedEvent(
+    val postId: Long
+) : ReportEvent
+
+class CommentReportedEvent(
+    val commentId: Long
+) : ReportEvent
+
+class ReplyReportedEvent(
+    val replyId: Long
+) : ReportEvent
+
