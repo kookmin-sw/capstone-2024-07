2da797da9e89915bb2aea709ab86b35705c0a153
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-05-04 21:19:30 +0900

    feat: spring batch 추가 및 설정을 추가한다

--- backend/src/main/kotlin/com/dclass/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/dclass/backend/BackendApplication.kt
@@ -1,13 +1,16 @@
 package com.dclass.backend
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableScheduling
+@EnableBatchProcessing
 class BackendApplication
 
 /**/
