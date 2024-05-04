afbd72726480e149a9d1064f628575833035d606
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-05-04 21:11:59 +0900

    feat: 신고받은 게시글, 댓글, 대댓글 작성자를 정지 시키는 기능을 구현한다

--- backend/src/main/kotlin/com/dclass/backend/application/ReportService.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/ReportService.kt
@@ -1,8 +1,17 @@
 package com.dclass.backend.application
 
 import com.dclass.backend.application.dto.UpdateReportRequest
+import com.dclass.backend.domain.blocklist.Blocklist
+import com.dclass.backend.domain.blocklist.BlocklistRepository
+import com.dclass.backend.domain.comment.CommentRepository
+import com.dclass.backend.domain.comment.getByIdOrThrow
+import com.dclass.backend.domain.post.PostRepository
+import com.dclass.backend.domain.post.findByIdOrThrow
+import com.dclass.backend.domain.reply.ReplyRepository
+import com.dclass.backend.domain.reply.getByIdOrThrow
 import com.dclass.backend.domain.report.ReportEvent
 import com.dclass.backend.domain.report.ReportRepository
+import com.dclass.backend.domain.report.ReportType
 import jakarta.transaction.Transactional
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
@@ -11,6 +20,10 @@ import org.springframework.stereotype.Service
 @Service
 class ReportService(
     private val reportRepository: ReportRepository,
+    private val blocklistRepository: BlocklistRepository,
+    private val commentRepository: CommentRepository,
+    private val replyRepository: ReplyRepository,
+    private val postRepository: PostRepository,
     private val eventPublisher: ApplicationEventPublisher
 ) {
     fun report(userId: Long, request: UpdateReportRequest) {
@@ -26,6 +39,26 @@ class ReportService(
         val reportCount = reportRepository.countReportById(request.reportedObjectId, request.reportType)
         if (reportCount >= 2) {
             eventPublisher.publishEvent(ReportEvent.create(request.reportedObjectId, request.reportType))
+            addBlocklist(request.reportedObjectId, request.reportType)
+        }
+    }
+
+    private fun addBlocklist(objectId: Long, type: ReportType) {
+        when (type) {
+            ReportType.COMMENT -> {
+                val comment = commentRepository.getByIdOrThrow(objectId)
+                blocklistRepository.save(Blocklist(comment.userId))
+            }
+
+            ReportType.REPLY -> {
+                val reply = replyRepository.getByIdOrThrow(objectId)
+                blocklistRepository.save(Blocklist(reply.userId))
+            }
+
+            else -> {
+                val post = postRepository.findByIdOrThrow(objectId)
+                blocklistRepository.save(Blocklist(post.userId))
+            }
         }
     }
 }
