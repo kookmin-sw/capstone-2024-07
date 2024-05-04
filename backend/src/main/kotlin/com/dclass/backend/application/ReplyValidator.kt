45cf30242e930d0b9e65083187b03aa86a43cd4d
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-05-04 21:14:34 +0900

    feat: 정지당한 유저가 활동을 하지 못하도록 설정한다

--- backend/src/main/kotlin/com/dclass/backend/application/ReplyValidator.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/ReplyValidator.kt
@@ -2,6 +2,9 @@ package com.dclass.backend.application
 
 import com.dclass.backend.domain.belong.BelongRepository
 import com.dclass.backend.domain.belong.getOrThrow
+import com.dclass.backend.domain.blocklist.BlocklistRepository
+import com.dclass.backend.exception.blocklist.BlocklistException
+import com.dclass.backend.exception.blocklist.BlocklistExceptionType
 import com.dclass.backend.exception.reply.ReplyException
 import com.dclass.backend.exception.reply.ReplyExceptionType
 import org.springframework.stereotype.Service
@@ -11,8 +14,11 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional
 class ReplyValidator(
     private val belongRepository: BelongRepository,
+    private val blocklistRepository: BlocklistRepository
 ) {
     fun validate(userId: Long, departmentId: Long) {
+        if (blocklistRepository.findByUserId(userId) != null) throw BlocklistException(BlocklistExceptionType.BLOCKED_USER)
+        
         val belong = belongRepository.getOrThrow(userId)
 
         if (!belong.contain(departmentId)) {
