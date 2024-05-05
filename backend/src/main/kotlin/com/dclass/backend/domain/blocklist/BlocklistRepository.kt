ce8474b3deabd78be308bc7e40a0ec9c2d8107eb
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-05-05 11:04:22 +0900

    feat: 쿼리를 추가 및 삭제한다

--- backend/src/main/kotlin/com/dclass/backend/domain/blocklist/BlocklistRepository.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/blocklist/BlocklistRepository.kt
@@ -3,14 +3,10 @@ package com.dclass.backend.domain.blocklist
 import com.dclass.backend.exception.blocklist.BlocklistException
 import com.dclass.backend.exception.blocklist.BlocklistExceptionType
 import org.springframework.data.jpa.repository.JpaRepository
-import java.time.LocalDateTime
 
-fun BlocklistRepository.getByUserIdOrThrow(userId: Long): Blocklist = findByUserId(userId)
+fun BlocklistRepository.getLatestByUserIdOrThrow(userId: Long): Blocklist = findFirstByUserIdOrderByCreatedDateTimeDesc(userId)
     ?: throw BlocklistException(BlocklistExceptionType.NOT_FOUND_USER)
 
 interface BlocklistRepository : JpaRepository<Blocklist, Long> {
-    fun findByUserId(userId: Long): Blocklist?
-    fun findAllByCreatedDateTimeBeforeAndDeletedIsFalse(dateTime: LocalDateTime): List<Blocklist>
-    fun deleteAllByCreatedDateTimeBefore(dateTime: LocalDateTime)
-
+    fun findFirstByUserIdOrderByCreatedDateTimeDesc(userId: Long): Blocklist?
 }
