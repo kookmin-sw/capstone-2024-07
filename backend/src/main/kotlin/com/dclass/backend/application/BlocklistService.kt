47a4a0feab387de3c1032d950163b656c1d64574
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-05-05 11:04:22 +0900

    feat: 쿼리를 추가 및 삭제한다

--- backend/src/main/kotlin/com/dclass/backend/application/BlocklistService.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/BlocklistService.kt
@@ -2,7 +2,7 @@ package com.dclass.backend.application
 
 import com.dclass.backend.application.dto.RemainDurationResponse
 import com.dclass.backend.domain.blocklist.BlocklistRepository
-import com.dclass.backend.domain.blocklist.getByUserIdOrThrow
+import com.dclass.backend.domain.blocklist.getLatestByUserIdOrThrow
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -12,7 +12,7 @@ class BlocklistService(
     private val blocklistRepository: BlocklistRepository
 ) {
     fun remain(userId: Long): RemainDurationResponse {
-        val blocklist = blocklistRepository.getByUserIdOrThrow(userId)
+        val blocklist = blocklistRepository.getLatestByUserIdOrThrow(userId)
         return RemainDurationResponse(blocklist.remainingTime)
     }
 }
