e67a602400a1a2c510d24470c9933ddbe67c7993
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-05-05 13:27:51 +0900

    refactor: flyway script를 변경한다

--- backend/src/main/resources/db/migration/V3__create_blocklist_table.sql
+++ backend/src/main/resources/db/migration/V3__create_blocklist_table.sql
@@ -3,6 +3,5 @@ create table blocklist
     id                bigint      not null auto_increment,
     user_id           bigint      not null,
     created_date_time datetime(6) not null,
-    deleted           bit         not null,
     primary key (id)
 ) engine = InnoDB;
