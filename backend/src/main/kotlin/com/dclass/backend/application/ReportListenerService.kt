5d217e0ebcc7d04e810d7787b05b1d31836f81ea
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-05-05 11:06:45 +0900

    refactor: blocklist 추가 로직을 수정한다

--- backend/src/main/kotlin/com/dclass/backend/application/ReportListenerService.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/ReportListenerService.kt
@@ -1,5 +1,7 @@
 package com.dclass.backend.application
 
+import com.dclass.backend.domain.blocklist.Blocklist
+import com.dclass.backend.domain.blocklist.BlocklistRepository
 import com.dclass.backend.domain.comment.CommentRepository
 import com.dclass.backend.domain.comment.getByIdOrThrow
 import com.dclass.backend.domain.post.PostRepository
@@ -9,10 +11,10 @@ import com.dclass.backend.domain.reply.getByIdOrThrow
 import com.dclass.backend.domain.report.CommentReportedEvent
 import com.dclass.backend.domain.report.PostReportedEvent
 import com.dclass.backend.domain.report.ReplyReportedEvent
-import org.springframework.transaction.annotation.Transactional
-import org.springframework.transaction.event.TransactionalEventListener
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.event.TransactionalEventListener
 
 
 @Service
@@ -20,33 +22,37 @@ import org.springframework.transaction.annotation.Propagation
 class ReportListenerService(
     private val postRepository: PostRepository,
     private val commentRepository: CommentRepository,
-    private val replyRepository: ReplyRepository
+    private val replyRepository: ReplyRepository,
+    private val blocklistRepository: BlocklistRepository
 ) {
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener
-    fun delete(event: PostReportedEvent){
+    fun delete(event: PostReportedEvent) {
         val post = postRepository.findByIdOrThrow(event.postId)
+        blocklistRepository.save(Blocklist(post.userId))
         postRepository.delete(post)
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener
-    fun delete(event: CommentReportedEvent){
+    fun delete(event: CommentReportedEvent) {
         val comment = commentRepository.getByIdOrThrow(event.commentId)
         val post = postRepository.findByIdOrThrow(comment.postId)
         post.decreaseCommentReplyCount()
+        blocklistRepository.save(Blocklist(comment.userId))
         commentRepository.delete(comment)
 
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener
-    fun delete(event: ReplyReportedEvent){
+    fun delete(event: ReplyReportedEvent) {
         val reply = replyRepository.getByIdOrThrow(event.replyId)
         val comment = commentRepository.getByIdOrThrow(reply.commentId)
         val post = postRepository.findByIdOrThrow(comment.postId)
         post.decreaseCommentReplyCount()
+        blocklistRepository.save(Blocklist(reply.userId))
         replyRepository.delete(reply)
     }
 }
