45041a8730de926a181a19f50d1ae1b9e1be8245
Woongbae Yun <devbelly@naver.com>
2024-05-28 10:26:27 +0900

    feat : @EnableRetry를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/dclass/backend/BackendApplication.kt
@@ -3,12 +3,14 @@ package com.dclass.backend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.retry.annotation.EnableRetry
 import org.springframework.scheduling.annotation.EnableScheduling
 
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableScheduling
+@EnableRetry
 class BackendApplication
 
 /**/
