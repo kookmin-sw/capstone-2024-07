106ad93fbf7b88d5d639a0c855c3fa42d48f4c9d
Woongbae Yun <devbelly@naver.com>
2024-07-02 13:16:05 +0900

    feat: Recruitment 애그리거트를 추가한다

---
+++ backend/src/main/kotlin/com/dclass/backend/domain/recruitment/Recruitment.kt
@@ -0,0 +1,87 @@
+package com.dclass.backend.domain.recruitment
+
+import com.dclass.support.domain.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Embedded
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.Table
+import jakarta.persistence.Version
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.SQLRestriction
+import java.time.LocalDateTime
+
+@SQLDelete(sql = "update Recruitment set deleted = true where id = ? and version = ?")
+@SQLRestriction("deleted = false")
+@Entity
+@Table
+class Recruitment(
+    @Column(nullable = false)
+    val userId: Long,
+
+    @Column(nullable = false)
+    val departmentId: Long,
+
+    type: RecruitmentType = RecruitmentType.STUDY,
+
+    isOnline: Boolean = false,
+
+    limit: RecruitmentNumber = RecruitmentNumber(-1),
+
+    recruitable: Boolean = false,
+
+    startDateTime: LocalDateTime,
+
+    endDateTime: LocalDateTime,
+
+    recruitmentLikes: RecruitmentLikes = RecruitmentLikes(),
+
+    title: String = "",
+
+    content: String = "",
+
+    anonymousNumber: Int = 0,
+
+    @Column(nullable = false)
+    val createdDateTime: LocalDateTime = LocalDateTime.now(),
+
+    @Version
+    val version: Long = 0L,
+
+    id: Long = 0L,
+) : BaseEntity(id) {
+    @Column(nullable = false)
+    var recruitable: Boolean = recruitable
+        private set
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    var type: RecruitmentType = type
+        private set
+
+    @Column(nullable = false)
+    var isOnline: Boolean = isOnline
+        private set
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    var limit: RecruitmentNumber = limit
+        private set
+
+    @Column(nullable = false)
+    var startDateTime: LocalDateTime = startDateTime
+        private set
+
+    @Column(nullable = false)
+    var endDateTime: LocalDateTime = endDateTime
+        private set
+
+    @Embedded
+    var recruitmentLikes: RecruitmentLikes = recruitmentLikes
+        private set
+
+    @Column(nullable = false)
+    var anonymousNumber: Int = anonymousNumber
+        private set
+}
---
+++ backend/src/main/kotlin/com/dclass/backend/domain/recruitment/RecruitmentType.kt
@@ -0,0 +1,5 @@
+package com.dclass.backend.domain.recruitment
+
+enum class RecruitmentType {
+    STUDY, PROJECT
+}
