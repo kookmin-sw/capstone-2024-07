554ba824b3400b08b84fff8b7c2b2dea4ab8eb09
hongbuly <hongbuly@naver.com>
2024-07-03 17:31:31 +0900

    feat : 친구초대 이벤트 배너 추가

---
+++ frontend/lib/board/layout/invite_layout.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frontend/common/const/colors.dart';
+
+class Invite extends ConsumerWidget {
+  const Invite({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 14,
+      ),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "\"컴퓨터공학과\"",
+                style: TextStyle(
+                  color: PRIMARY_COLOR,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                " 친구들을 초대해주세요!",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          const Text(
+            "친구 5명이 추천아이디 입력시 스타벅스 쿠폰을 드립니다.",
+            style: TextStyle(
+              fontSize: 10,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          const SizedBox(
+            height: 7,
+          ),
+          GestureDetector(
+            onTap: () {
+              // TODO : show about event
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 5,
+                    horizontal: 18,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: ACTIVE_COLOR,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "더 알아보기",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 11,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
