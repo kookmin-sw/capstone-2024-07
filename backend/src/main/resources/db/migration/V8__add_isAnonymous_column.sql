9aa9047a4c6088e881f6f89c3439a7bf4b8adbf0
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-07-03 21:28:09 +0900

    feat: Anonymous migration script를 작성한다

--- backend/src/main/resources/db/migration/V8__add_isAnonymous_column.sql
+++ backend/src/main/resources/db/migration/V8__add_isAnonymous_column.sql
@@ -1,3 +1,12 @@
+create table anonymous
+(
+    id                bigint      not null auto_increment,
+    user_id           bigint      not null,
+    post_id           bigint      not null,
+    deleted           bit         not null,
+    primary key (id)
+) engine = InnoDB;
+
 ALTER TABLE post ADD COLUMN is_anonymous BOOLEAN DEFAULT FALSE NOT NULL;
 
 ALTER TABLE comment ADD COLUMN is_anonymous BOOLEAN DEFAULT FALSE NOT NULL;
