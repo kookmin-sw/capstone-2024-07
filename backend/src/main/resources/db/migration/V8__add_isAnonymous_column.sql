12ecedb98aa227bc199adf66f7d522442231f887
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-07-05 14:44:45 +0900

    feat: deleted column을 제거한다

--- backend/src/main/resources/db/migration/V8__add_isAnonymous_column.sql
+++ backend/src/main/resources/db/migration/V8__add_isAnonymous_column.sql
@@ -3,7 +3,6 @@ create table anonymous
     id                bigint      not null auto_increment,
     user_id           bigint      not null,
     post_id           bigint      not null,
-    deleted           bit         not null,
     primary key (id)
 ) engine = InnoDB;
 
