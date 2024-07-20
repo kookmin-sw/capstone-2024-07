cd9fc5f32b918134727255049f1c8e49eb7e1e21
hongbuly <hongbuly@naver.com>
2024-07-20 09:23:20 +0900

    feat : 카카오톡 공유 기능 연결

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kakao_flutter_sdk_share/kakao_flutter_sdk_share.dart';
 
 import 'common/provider/router_provider.dart';
 import 'package:shorebird_code_push/shorebird_code_push.dart';
@@ -7,6 +8,9 @@ import 'package:shorebird_code_push/shorebird_code_push.dart';
 final shorebirdCodePush = ShorebirdCodePush();
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  KakaoSdk.init(nativeAppKey: '38885f51878b5df2c916adbdf3f91132');
+
   runApp(
     const ProviderScope(
       child: _App(),
