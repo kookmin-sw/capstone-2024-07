f2fa4865b264d1badb3faed0b83f8785dbfb8053
hongbuly <hongbuly@naver.com>
2024-07-20 09:23:20 +0900

    feat : 카카오톡 공유 기능 연결

--- frontend/lib/board/layout/invite_detail_layout.dart
+++ frontend/lib/board/layout/invite_detail_layout.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:frontend/common/const/colors.dart';
+import 'package:kakao_flutter_sdk_share/kakao_flutter_sdk_share.dart';
 
 class InviteDetailLayout extends ConsumerWidget {
   const InviteDetailLayout({super.key});
+
+  void notAllowed(BuildContext context, String s) {
+    showDialog(
+        context: context,
+        builder: ((context) {
+          return AlertDialog(
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                  child: Text(
+                    s,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 13,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: PRIMARY50_COLOR,
+                    ),
+                    child: const Text(
+                      "확인",
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: PRIMARY_COLOR,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          );
+        }));
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -179,30 +230,85 @@ class InviteDetailLayout extends ConsumerWidget {
               const SizedBox(
                 height: 15,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 5,
-                      horizontal: 18,
-                    ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: ACTIVE_COLOR,
-                    ),
-                    child: const Center(
-                      child: Text(
-                        "친구에게 알리기",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 11,
+              GestureDetector(
+                onTap: () async {
+                  bool isKakaoTalkSharingAvailable =
+                      await ShareClient.instance.isKakaoTalkSharingAvailable();
+                  if (isKakaoTalkSharingAvailable) {
+                    final FeedTemplate defaultFeed = FeedTemplate(
+                      content: Content(
+                        title: "Decl(디클)",
+                        description: "대학교 학과별 커뮤니티",
+                        imageUrl: Uri.parse(
+                            'https://private-user-images.githubusercontent.com/67682840/324183234-a6255c3f-ff7a-43b7-8e14-7e1199be3ac0.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjA5NTE3NzAsIm5iZiI6MTcyMDk1MTQ3MCwicGF0aCI6Ii82NzY4Mjg0MC8zMjQxODMyMzQtYTYyNTVjM2YtZmY3YS00M2I3LThlMTQtN2UxMTk5YmUzYWMwLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA3MTQlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNzE0VDEwMDQzMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWE4ZWFlNjVkODc5NTYyODIzZDFjYjBhNWYxZjVmNDE2OTdmZGZlMDNjYWVhNWUxMzI0M2U2MjRhOTc2ZDc3YjUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.Z8rbxgp2ii9wIIM418ku47W64_mp1oFJGgMDZKnmr78'),
+                        link: Link(
+                          androidExecutionParams: {
+                            'key1': 'value1',
+                            'key2': 'value2'
+                          },
+                          iosExecutionParams: {
+                            'key1': 'value1',
+                            'key2': 'value2'
+                          },
+                        ),
+                      ),
+                      buttons: [
+                        Button(
+                          title: "앱 다운로드 받기",
+                          link: Link(
+                            androidExecutionParams: {
+                              'key1': 'value1',
+                              'key2': 'value2'
+                            },
+                            iosExecutionParams: {
+                              'key1': 'value1',
+                              'key2': 'value2'
+                            },
+                          ),
+                        ),
+                      ],
+                    );
+
+                    try {
+                      Uri uri = await ShareClient.instance
+                          .shareDefault(template: defaultFeed);
+                      await ShareClient.instance.launchKakaoTalk(uri);
+                    } catch (error) {
+                      debugPrint("KakaoTalk Error : $error");
+                    }
+                  } else {
+                    debugPrint("KakaoTalk not Installed.");
+                    Clipboard.setData(const ClipboardData(
+                        text:
+                            "Decl(디클)\n대학교 학과별 커뮤니티\n\nIOS\nhttps://apps.apple.com/kr/app/%EB%94%94%ED%81%B4/id6499332881\n\nANDROID\nhttps://play.google.com/store/apps/details?id=com.capstone.decl&pcampaignid=web_share"));
+                    notAllowed(context, "링크가 복사되었습니다.");
+                  }
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 18,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: ACTIVE_COLOR,
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "친구에게 알리기",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 11,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ],
           ),
