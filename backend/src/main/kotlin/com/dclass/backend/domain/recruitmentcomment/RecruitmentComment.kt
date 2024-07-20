f4b701f6bccdfb096c537ee5b0ac68328e0861e5
Woongbae Yun <devbelly@naver.com>
2024-07-20 10:55:09 +0900

    fix: 테이블 미추가로 변경한다

--- backend/src/main/kotlin/com/dclass/backend/domain/recruitmentcomment/RecruitmentComment.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/recruitmentcomment/RecruitmentComment.kt
@@ -1,47 +1,47 @@
-package com.dclass.backend.domain.recruitmentcomment
-
-import com.dclass.support.domain.BaseEntity
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Version
-import org.hibernate.annotations.SQLDelete
-import org.hibernate.annotations.SQLRestriction
-import java.time.LocalDateTime
-
-@SQLDelete(sql = "update recruitment_comment set deleted = true where id = ? and version = ?")
-@SQLRestriction("deleted = false OR (deleted = true AND reply_count > 0)")
-@Entity
-class RecruitmentComment(
-    @Column(nullable = false)
-    val userId: Long,
-
-    @Column(nullable = false)
-    val recruitmentId: Long,
-
-    content: String = "",
-
-    @Column(nullable = false)
-    val createdDateTime: LocalDateTime = LocalDateTime.now(),
-
-    @Version
-    val version: Long = 0L,
-
-    modifiedDateTime: LocalDateTime = LocalDateTime.now(),
-
-    id: Long = 0L,
-) : BaseEntity(id) {
-    @Column(nullable = false)
-    private var deleted: Boolean = false
-
-    @Column(nullable = false, length = 255)
-    var content: String = content
-        private set
-
-    @Column(nullable = false)
-    var modifiedDateTime: LocalDateTime = modifiedDateTime
-        private set
-
-    @Column(nullable = false)
-    var replyCount: Int = 0
-        private set
-}
+//package com.dclass.backend.domain.recruitmentcomment
+//
+//import com.dclass.support.domain.BaseEntity
+//import jakarta.persistence.Column
+//import jakarta.persistence.Entity
+//import jakarta.persistence.Version
+//import org.hibernate.annotations.SQLDelete
+//import org.hibernate.annotations.SQLRestriction
+//import java.time.LocalDateTime
+//
+//@SQLDelete(sql = "update recruitment_comment set deleted = true where id = ? and version = ?")
+//@SQLRestriction("deleted = false OR (deleted = true AND reply_count > 0)")
+//@Entity
+//class RecruitmentComment(
+//    @Column(nullable = false)
+//    val userId: Long,
+//
+//    @Column(nullable = false)
+//    val recruitmentId: Long,
+//
+//    content: String = "",
+//
+//    @Column(nullable = false)
+//    val createdDateTime: LocalDateTime = LocalDateTime.now(),
+//
+//    @Version
+//    val version: Long = 0L,
+//
+//    modifiedDateTime: LocalDateTime = LocalDateTime.now(),
+//
+//    id: Long = 0L,
+//) : BaseEntity(id) {
+//    @Column(nullable = false)
+//    private var deleted: Boolean = false
+//
+//    @Column(nullable = false, length = 255)
+//    var content: String = content
+//        private set
+//
+//    @Column(nullable = false)
+//    var modifiedDateTime: LocalDateTime = modifiedDateTime
+//        private set
+//
+//    @Column(nullable = false)
+//    var replyCount: Int = 0
+//        private set
+//}
