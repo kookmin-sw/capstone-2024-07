ad58c8bf3caa5b06d7c88cc038a93fd5de2c7eb8
Woongbae Yun <devbelly@naver.com>
2024-07-20 10:37:09 +0900

    feat: RecruitmentDto를 추가한다

---
+++ backend/src/main/kotlin/com/dclass/backend/application/dto/RecruitmentDtos.kt
@@ -0,0 +1,143 @@
+package com.dclass.backend.application.dto
+
+import com.dclass.backend.domain.hashtag.HashTag
+import com.dclass.backend.domain.recruitment.Recruitment
+import com.dclass.backend.domain.recruitment.RecruitmentNumber
+import com.dclass.backend.domain.recruitment.RecruitmentType
+import com.dclass.backend.domain.user.User
+import java.time.LocalDateTime
+
+data class CreateRecruitmentRequest(
+    val type: RecruitmentType,
+    val isOnline: Boolean,
+    val isOngoing: Boolean,
+    val limit: Int,
+    val startDateTime: LocalDateTime,
+    val endDateTime: LocalDateTime,
+    var title: String,
+    var content: String,
+    val hashTags: List<String>,
+) {
+    fun toRecruitment(userId: Long, departmentId: Long): Recruitment {
+        return Recruitment(
+            userId = userId,
+            departmentId = departmentId,
+            type = type,
+            isOnline = isOnline,
+            isOngoing = isOngoing,
+            limit = RecruitmentNumber(limit),
+            startDateTime = startDateTime,
+            endDateTime = endDateTime,
+            title = title,
+            content = content,
+        )
+    }
+}
+
+data class RecruitmentResponse(
+    val id: Long,
+    val userId: Long,
+    val title: String,
+    val content: String,
+) {
+    constructor(recruitment: Recruitment) : this(
+        id = recruitment.id,
+        userId = recruitment.userId,
+        title = recruitment.title,
+        content = recruitment.content,
+    )
+}
+
+data class RecruitmentScrollPageRequest(
+    val lastId: Long? = null,
+    val size: Int = 10,
+)
+
+data class RecruitmentWithUserResponse(
+    val id: Long,
+    val departmentId: Long,
+    val type: RecruitmentType,
+    val isOnline: Boolean,
+    val isOngoing: Boolean,
+    val limit: Int,
+    val recruitable: Boolean,
+    val startDateTime: LocalDateTime,
+    val endDateTime: LocalDateTime,
+    val title: String,
+    val content: String,
+    val scrapCount: Int,
+    val createdDateTime: LocalDateTime,
+    val modifiedDateTime: LocalDateTime,
+    val userId: Long,
+    val userNickname: String,
+) {
+    constructor(recruitment: Recruitment, user: User) : this(
+        recruitment.id,
+        recruitment.departmentId,
+        recruitment.type,
+        recruitment.isOnline,
+        recruitment.isOngoing,
+        recruitment.limit.number,
+        recruitment.recruitable,
+        recruitment.startDateTime,
+        recruitment.endDateTime,
+        recruitment.title,
+        recruitment.content,
+        recruitment.scrapCount,
+        recruitment.createdDateTime,
+        recruitment.modifiedDateTime,
+        recruitment.userId,
+        user.nickname,
+    )
+}
+
+data class RecruitmentWithUserAndHashTagResponse(
+    val id: Long,
+    val departmentId: Long,
+    val type: RecruitmentType,
+    val isOnline: Boolean,
+    val isOngoing: Boolean,
+    val limit: Int,
+    val recruitable: Boolean,
+    val startDateTime: LocalDateTime,
+    val endDateTime: LocalDateTime,
+    val title: String,
+    val content: String,
+    val scrapCount: Int,
+    val createdDateTime: LocalDateTime,
+    val modifiedDateTime: LocalDateTime,
+    val userId: Long,
+    val userNickname: String,
+    val hashTags: List<String>,
+) {
+    constructor(recruitment: RecruitmentWithUserResponse, hashTags: List<HashTag>) : this(
+        recruitment.id,
+        recruitment.departmentId,
+        recruitment.type,
+        recruitment.isOnline,
+        recruitment.isOngoing,
+        recruitment.limit,
+        recruitment.recruitable,
+        recruitment.startDateTime,
+        recruitment.endDateTime,
+        recruitment.title,
+        recruitment.content,
+        recruitment.scrapCount,
+        recruitment.createdDateTime,
+        recruitment.modifiedDateTime,
+        recruitment.userId,
+        recruitment.userNickname,
+        hashTags.map { it.name },
+    )
+}
+
+data class RecruitmentsResponse(
+    val data: List<RecruitmentWithUserAndHashTagResponse>,
+    val meta: MetaData,
+) {
+    companion object {
+        fun of(data: List<RecruitmentWithUserAndHashTagResponse>, limit: Int): RecruitmentsResponse {
+            return RecruitmentsResponse(data, MetaData(data.size, data.size >= limit))
+        }
+    }
+}
