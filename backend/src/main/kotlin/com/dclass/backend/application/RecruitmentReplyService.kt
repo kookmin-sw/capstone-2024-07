e9fd710524255e1eac208531c696ac4f549093ea
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-07-21 22:04:33 +0900

    feat: Recruitment 댓글 & 대댓글 관련 로직을 구현한다

---
+++ backend/src/main/kotlin/com/dclass/backend/application/RecruitmentReplyService.kt
@@ -0,0 +1,75 @@
+package com.dclass.backend.application
+
+import com.dclass.backend.application.dto.CreateRecruitmentReplyRequest
+import com.dclass.backend.application.dto.DeleteRecruitmentReplyRequest
+import com.dclass.backend.application.dto.RecruitmentReplyResponse
+import com.dclass.backend.application.dto.UpdateRecruitmentReplyRequest
+import com.dclass.backend.domain.recruitment.RecruitmentRepository
+import com.dclass.backend.domain.recruitment.findByIdOrThrow
+import com.dclass.backend.domain.recruitmentcomment.RecruitmentCommentRepository
+import com.dclass.backend.domain.recruitmentcomment.getByIdOrThrow
+import com.dclass.backend.domain.recruitmentreply.RecruitmentReplyRepository
+import com.dclass.backend.domain.recruitmentreply.getByIdAndUserIdOrThrow
+import com.dclass.backend.exception.comment.CommentException
+import com.dclass.backend.exception.comment.CommentExceptionType
+import org.springframework.orm.ObjectOptimisticLockingFailureException
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+@Service
+class RecruitmentReplyService(
+    private val replyRepository: RecruitmentReplyRepository,
+    private val recruitmentRepository: RecruitmentRepository,
+    private val commentRepository: RecruitmentCommentRepository,
+    private val validator: CommentReplyValidator
+) {
+    @Retryable(
+        ObjectOptimisticLockingFailureException::class,
+        maxAttempts = 3,
+        backoff = Backoff(delay = 500),
+    )
+    fun create(userId: Long, request: CreateRecruitmentReplyRequest): RecruitmentReplyResponse {
+        val comment = commentRepository.getByIdOrThrow(request.commentId)
+
+        if(comment.isDeleted()) {
+            throw CommentException(CommentExceptionType.DELETED_COMMENT)
+        }
+
+        val recruitment = recruitmentRepository.findByIdOrThrow(comment.recruitmentId)
+
+        validator.validate(userId, recruitment.departmentId)
+
+        val reply = replyRepository.save(request.toEntity(userId))
+
+        // TODO : 익명 여부
+
+        comment.increaseReplyCount()
+        recruitment.increaseCommentReplyCount()
+
+        return RecruitmentReplyResponse(reply)
+    }
+
+    fun update(userId: Long, request: UpdateRecruitmentReplyRequest) {
+        val reply = replyRepository.getByIdAndUserIdOrThrow(request.replyId, userId)
+        reply.changeContent(request.content)
+    }
+
+    @Retryable(
+        ObjectOptimisticLockingFailureException::class,
+        maxAttempts = 3,
+        backoff = Backoff(delay = 500),
+    )
+    fun delete(userId: Long, request: DeleteRecruitmentReplyRequest) {
+        val reply = replyRepository.getByIdAndUserIdOrThrow(request.replyId, userId)
+        val comment = commentRepository.getByIdOrThrow(reply.recruitmentCommentId)
+        val recruitment = recruitmentRepository.findByIdOrThrow(comment.recruitmentId)
+
+        comment.decreaseReplyCount()
+        recruitment.decreaseCommentReplyCount()
+
+        replyRepository.delete(reply)
+    }
+}
