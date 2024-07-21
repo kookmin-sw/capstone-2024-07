11e0c3661341a2423987a50c09b7723b24ac4833
Woongbae Yun <devbelly@naver.com>
2024-07-21 11:11:31 +0900

    feat: 단건조회 확장함수를 작성한다

--- backend/src/main/kotlin/com/dclass/backend/domain/recruitment/RecruitmentRepository.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/recruitment/RecruitmentRepository.kt
@@ -1,5 +1,12 @@
 package com.dclass.backend.domain.recruitment
 
+import com.dclass.backend.exception.recruitment.RecruitmentException
+import com.dclass.backend.exception.recruitment.RecruitmentExceptionType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.findByIdOrNull
+
+fun RecruitmentRepository.findByIdOrThrow(id: Long): Recruitment {
+    return findByIdOrNull(id) ?: throw RecruitmentException(RecruitmentExceptionType.NOT_FOUND_RECRUITMENT)
+}
 
 interface RecruitmentRepository : JpaRepository<Recruitment, Long>, RecruitmentRepositorySupport
