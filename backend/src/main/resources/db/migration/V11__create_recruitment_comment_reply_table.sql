10113c511920fe0a8ca9ff89af199fa8bed215fd
Dongyun Kim <102042061+zkxmdkdltm@users.noreply.github.com>
2024-07-21 22:18:05 +0900

    feat: migration script를 작성한다

---
+++ backend/src/main/resources/db/migration/V11__create_recruitment_comment_reply_table.sql
@@ -0,0 +1,26 @@
+create table recruitment_comment
+(
+    id                  bigint       not null auto_increment,
+    user_id             bigint       not null,
+    recruitment_id      bigint       not null,
+    reply_count         integer      not null,
+    content             varchar(255) not null,
+    created_date_time   datetime(6)  not null,
+    modified_date_time  datetime(6)  not null,
+    version             bigint       not null,
+    deleted             bit          not null,
+    primary key (id)
+);
+
+create table recruitment_reply
+(
+    id                      bigint       not null auto_increment,
+    user_id                 bigint       not null,
+    recruitment_comment_id  bigint       not null,
+    content                 varchar(255) not null,
+    created_date_time       datetime(6)  not null,
+    modified_date_time      datetime(6)  not null,
+    deleted                 bit          not null,
+    primary key (id)
+);
+)
