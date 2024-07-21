8968e4990a0954a07cb4ff5d1f12ad96f01f8a84
Woongbae Yun <devbelly@naver.com>
2024-07-21 11:07:08 +0900

    feat: 해시태그 조회 쿼리를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/domain/hashtag/HashTagRepository.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/hashtag/HashTagRepository.kt
@@ -2,7 +2,8 @@ package com.dclass.backend.domain.hashtag
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface HashTagRepository : JpaRepository<HashTag, Long> {
+interface HashTagRepository : JpaRepository<HashTag, Long>, HashTagRepositorySupport {
 
     fun findRecruitmentHashTagByTargetAndTargetIdIn(target: HashTagTarget, targetIds: List<Long>): List<HashTag>
+    fun findRecruitmentHashTagByTargetAndTargetId(target: HashTagTarget, targetId: Long): List<HashTag>
 }
