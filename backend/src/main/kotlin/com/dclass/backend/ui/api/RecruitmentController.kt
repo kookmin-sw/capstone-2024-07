c8a6f905931d6c23dcb549388c48482ae7b973b7
Woongbae Yun <devbelly@naver.com>
2024-07-23 15:12:49 +0900

    feat: 스웨거 설정을 추가한다

--- backend/src/main/kotlin/com/dclass/backend/ui/api/RecruitmentController.kt
+++ backend/src/main/kotlin/com/dclass/backend/ui/api/RecruitmentController.kt
@@ -10,6 +10,9 @@ import com.dclass.backend.application.dto.RecruitmentsResponse
 import com.dclass.backend.application.dto.UpdateRecruitmentRequest
 import com.dclass.backend.domain.user.User
 import com.dclass.backend.security.LoginUser
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -20,12 +23,15 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Recruitment", description = "모집 관련 API 명세")
 @RequestMapping("/api/recruitment")
 @RestController
 class RecruitmentController(
     private val recruitmentService: RecruitmentService,
 ) {
 
+    @Operation(summary = "모집 조회 API", description = "모집을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "모집 조회 성공")
     @GetMapping("/{recruitmentId}")
     fun getRecruitment(
         @LoginUser user: User,
@@ -35,6 +41,8 @@ class RecruitmentController(
         return ResponseEntity.ok(recruitmentResponse)
     }
 
+    @Operation(summary = "모집 목록 조회 API", description = "모집 목록을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "모집 목록 조회 성공")
     @GetMapping
     fun getRecruitments(
         @LoginUser user: User,
@@ -43,6 +51,8 @@ class RecruitmentController(
         return ResponseEntity.ok(recruitmentService.getAll(user.id, request))
     }
 
+    @Operation(summary = "내 모집 목록 조회 API", description = "내 모집 목록을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "내 모집 목록 조회 성공")
     @GetMapping("/mine")
     fun getMyRecruitments(
         @LoginUser user: User,
@@ -51,6 +61,8 @@ class RecruitmentController(
         return ResponseEntity.ok(recruitmentService.getByUserId(user.id, request))
     }
 
+    @Operation(summary = "스크랩한 모집 목록 조회 API", description = "스크랩한 모집 목록을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "스크랩한 모집 목록 조회 성공")
     @GetMapping("/scrapped")
     fun getScrappedRecruitments(
         @LoginUser user: User,
@@ -61,6 +73,8 @@ class RecruitmentController(
 
     // TODO getCommentedRecruitments
 
+    @Operation(summary = "모집 생성 API", description = "모집을 생성합니다.")
+    @ApiResponse(responseCode = "200", description = "모집 생성 성공")
     @PostMapping
     fun createRecruitment(
         @LoginUser user: User,
@@ -69,6 +83,8 @@ class RecruitmentController(
         return ResponseEntity.ok(recruitmentService.create(user.id, request))
     }
 
+    @Operation(summary = "모집 수정 API", description = "모집을 수정합니다.")
+    @ApiResponse(responseCode = "200", description = "모집 수정 성공")
     @PutMapping
     fun updateRecruitment(
         @LoginUser user: User,
@@ -77,6 +93,8 @@ class RecruitmentController(
         return ResponseEntity.ok(recruitmentService.update(user.id, request))
     }
 
+    @Operation(summary = "모집 삭제 API", description = "모집을 삭제합니다.")
+    @ApiResponse(responseCode = "204", description = "모집 삭제 성공")
     @DeleteMapping("/{recruitmentId}")
     fun deleteRecruitment(
         @LoginUser user: User,
