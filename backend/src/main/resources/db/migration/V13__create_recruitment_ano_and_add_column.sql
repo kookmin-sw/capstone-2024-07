d2bc7a411ca8805fac4be4f4f5dd33f15ded38ec
Woongbae Yun <devbelly@naver.com>
2024-08-14 09:38:59 +0900

    feat: RecruitmentAnonymous 엔티티를 추가한다

---
+++ backend/src/main/resources/db/migration/V13__create_recruitment_ano_and_add_column.sql
@@ -0,0 +1,7 @@
+create table recruitment_anonymous
+(
+    id             bigint not null auto_increment,
+    recruitment_id bigint not null,
+    user_id        bigint not null,
+    primary key (id)
+) engine=InnoDB;
