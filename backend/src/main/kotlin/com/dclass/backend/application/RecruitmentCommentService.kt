ec241aef881af27cc5e7e33ed76eaa311837490b
Woongbae Yun <devbelly@naver.com>
2024-08-14 10:09:34 +0900

    feat: 댓글과 대댓글의 익명 여부와 차단 여부를 추가한다

--- backend/src/main/kotlin/com/dclass/backend/application/RecruitmentCommentService.kt
+++ backend/src/main/kotlin/com/dclass/backend/application/RecruitmentCommentService.kt
@@ -9,9 +9,12 @@ import com.dclass.backend.application.dto.RecruitmentCommentsResponse
 import com.dclass.backend.application.dto.UpdateRecruitmentCommentRequest
 import com.dclass.backend.domain.recruitment.RecruitmentRepository
 import com.dclass.backend.domain.recruitment.findByIdOrThrow
+import com.dclass.backend.domain.recruitmentanonymous.RecruitmentAnonymous
+import com.dclass.backend.domain.recruitmentanonymous.RecruitmentAnonymousRepository
 import com.dclass.backend.domain.recruitmentcomment.RecruitmentCommentRepository
 import com.dclass.backend.domain.recruitmentcomment.getByIdAndUserIdOrThrow
 import com.dclass.backend.domain.recruitmentreply.RecruitmentReplyRepository
+import com.dclass.backend.domain.userblock.UserBlockRepository
 import com.dclass.backend.exception.comment.CommentException
 import com.dclass.backend.exception.comment.CommentExceptionType
 import org.springframework.orm.ObjectOptimisticLockingFailureException
@@ -27,6 +30,8 @@ class RecruitmentCommentService(
     private val replyRepository: RecruitmentReplyRepository,
     private val recruitmentRepository: RecruitmentRepository,
     private val validator: CommentReplyValidator,
+    private val userBlockRepository: UserBlockRepository,
+    private val anonymousRepository: RecruitmentAnonymousRepository
 ) {
     @Retryable(
         ObjectOptimisticLockingFailureException::class,
@@ -37,6 +42,10 @@ class RecruitmentCommentService(
         val recruitment = recruitmentRepository.findByIdOrThrow(request.recruitmentId)
         validator.validate(userId, recruitment.departmentId)
         val comment = commentRepository.save(request.toEntity(userId))
+
+        if (request.isAnonymous && !anonymousRepository.existsByUserIdAndRecruitmentId(userId, recruitment.id)) {
+            anonymousRepository.save(RecruitmentAnonymous(userId, recruitment.id))
+        }
 
         recruitment.increaseCommentReplyCount()
 
@@ -65,9 +74,32 @@ class RecruitmentCommentService(
 
     @Transactional(readOnly = true)
     fun findAll(userId: Long, request: RecruitmentCommentScrollRequest): RecruitmentCommentsResponse {
+        val blockedUserIds = userBlockRepository.findByBlockerUserId(userId).map { it.blockedUserId }
+        val anonymousList = anonymousRepository.findByRecruitmentId(request.recruitmentId)
         val comments = commentRepository.findRecruitmentCommentWithUserByRecruitmentId(request)
+            .map { it.apply { isBlockedUser = blockedUserIds.contains(it.userId) } }
+            .map {
+                it.apply {
+                    this.userInformation.updateAnonymous(
+                        getIndex(anonymousList, this.userId),
+                        this.isAnonymous,
+                        this.userId == userId,
+                    )
+                }
+            }
+
         val commentIds = comments.map { it.id }
         val replies = replyRepository.findRecruitmentRepliesWithUserByCommentIdIn(commentIds)
+            .map { it.apply { isBlockedUser = blockedUserIds.contains(it.userId) } }
+            .map {
+                it.apply {
+                    this.userInformation.updateAnonymous(
+                        getIndex(anonymousList, this.userId),
+                        this.isAnonymous,
+                        this.userId == userId,
+                    )
+                }
+            }
             .groupBy { it.commentId }
         val data = comments.map {
             RecruitmentCommentReplyWithUserResponse(
@@ -76,8 +108,10 @@ class RecruitmentCommentService(
             )
         }
 
-        // TODO : 익명 여부, 차단 여부
-
         return RecruitmentCommentsResponse.of(data, request.size)
+    }
+
+    private fun getIndex(anonymousList: List<RecruitmentAnonymous>, userId: Long): Int {
+        return anonymousList.indexOfFirst { it.userId == userId }
     }
 }
