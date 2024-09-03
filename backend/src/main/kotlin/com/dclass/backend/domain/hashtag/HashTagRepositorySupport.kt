c5e9461ee4a1d6b1d4d29abdda31b60ac9463e29
Woongbae Yun <devbelly@naver.com>
2024-09-03 10:43:30 +0900

    fix: 해시태그가 존재할 때만 제거하도록 변경한다

--- backend/src/main/kotlin/com/dclass/backend/domain/hashtag/HashTagRepositorySupport.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/hashtag/HashTagRepositorySupport.kt
@@ -1,6 +1,8 @@
 package com.dclass.backend.domain.hashtag
 
+import com.linecorp.kotlinjdsl.dsl.jpql.Jpql
 import com.linecorp.kotlinjdsl.dsl.jpql.jpql
+import com.linecorp.kotlinjdsl.querymodel.jpql.predicate.Predicatable
 import com.linecorp.kotlinjdsl.render.jpql.JpqlRenderContext
 import com.linecorp.kotlinjdsl.support.spring.data.jpa.extension.createQuery
 import jakarta.persistence.EntityManager
@@ -16,9 +18,15 @@ class HashTagRepositoryImpl(
     override fun deleteAllByIdIn(ids: List<Long>): Int {
         val query = jpql {
             deleteFrom(entity(HashTag::class))
-                .where(path(HashTag::id).`in`(ids))
+                .where(
+                    idExist(ids),
+                )
         }
 
         return em.createQuery(query, context).executeUpdate()
+    }
+
+    private fun Jpql.idExist(request: List<Long>): Predicatable? {
+        return if (request.isNotEmpty()) (path(HashTag::id).`in`(request)) else null
     }
 }
